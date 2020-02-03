ff3f937623cff8a6ad380c2c1a7b20ad40d0bcee
zr <247076126@qq.com>
2020-02-03 12:52:11 +0800

    清空不需要

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  }, mounted () {
+    console.log(process.env);
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
