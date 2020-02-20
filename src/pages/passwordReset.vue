39db53dc642c0c9a48c208e2f3e583ed9b3deb12
zgw <zgw@qq.com>
2020-02-20 14:16:41 +0800

    密码重置模块修改

--- src/pages/passwordReset.vue
+++ src/pages/passwordReset.vue
@@ -3,178 +3,205 @@
     width: 200px;
     margin-right: 20px;
     box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);
-    padding: 0 15px 15px;
+    padding: 15px 15px 15px;
     border-radius: 10px;
-    /deep/.el-input {
+
+    /deep/ .el-input {
       margin: 10px 0 10px;
     }
-  }
-</style>
 
-<template v-slot:tree>
-  <div class="elTree">
-    <el-input placeholder="输入关键字进行过滤" v-model="filterText">
-    </el-input>
-    <el-tree
-      ref="tree2"
-      :data="treeData"
-      :default-expanded-keys="[1]"
-      node-key="id"
-      show-checkbox
-      :props="defaultProps"
-      @check-change="checkChange"
-      @check="checkcheck"
-      :highlight-current="true"
-      :filter-node-method="filterNode"
-      @node-click="handleNodeClick"
-    ></el-tree>
+    float: left;
+  }
+
+  .personSelect{
+    float: left;
+  }
+
+  .formItem{
+    margin: 0 10px;
+    display: inline-block;
+  }
+
+</style>
+<template>
+  <div>
+    <template>
+      <div class="elTree">
+<!--        <el-input placeholder="输入关键字进行过滤" v-model="filterText">-->
+<!--        </el-input>-->
+        <el-tree
+          ref="tree2"
+          :data="treeData"
+          :default-expanded-keys="[1]"
+          node-key="nodeId"
+          show-checkbox
+          :props="defaultProps"
+          @check-change="checkChange"
+          @check="treeCheck"
+          :highlight-current="true"
+          :filter-node-method="filterNode"
+        ></el-tree>
+      </div>
+    </template>
+    <template>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>人员密码重置</span>
+        </div>
+        <div class="text item">
+          <div class="formItem">
+            人员:
+            <el-select v-model="personBusinessId" placeholder="请选择">
+              <el-option
+                v-for="item in employeeList"
+                :key="item.nodeId"
+                :label="item.labelName"
+                :value="item.businessId">
+              </el-option>
+            </el-select>
+          </div>
+          <div class="formItem">
+            最新密码:
+            <el-input v-model="newPwd" style="width: 200px;" placeholder="最新密码" disabled></el-input>
+          </div>
+          <div class="formItem">
+            <el-button type="primary" @click="resetPwd">重置密码</el-button>
+          </div>
+        </div>
+      </el-card>
+    </template>
   </div>
 </template>
-
-<template>
-  <el-select v-model="value" placeholder="请选择">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select>
-</template>
-
 <script>
-  import listPage from "@/components/listPage";
   export default {
     watch: {
       filterText(val) {
         this.$refs.tree2.filter(val);
       }
     },
-    components: {
-      listPage
-    },
+    components: {},
     data() {
       return {
-        treeData: [
-          {
-            id: 1,
-            label: "总公司",
-            children: [
-              {
-                id: 2,
-                label: "xx区域1",
-                children: [
-                  {
-                    id: 3,
-                    label: "xx门店",
-                    children: [
-                      {
-                        id: 4,
-                        label: "xx经纪人"
-                      },
-                      {
-                        id: 44,
-                        label: "xx经纪人"
-                      }
-                    ]
-                  }
-                ]
-              },
-              {
-                id: 5,
-                label: "xx区域2",
-                children: [
-                  {
-                    id: 6,
-                    label: "xx门店",
-                    children: [
-                      {
-                        id: 7,
-                        label: "xx经纪人"
-                      }
-                    ]
-                  }
-                ]
-              },
-              {
-                id: 8,
-                label: "xx区域2",
-                children: [
-                  {
-                    id: 9,
-                    label: "xx门店",
-                    children: [
-                      {
-                        id: 10,
-                        label: "xx经纪人"
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          }
-        ],
+        treeData: [],
         filterText: "",
         defaultProps: {
-          children: "children",
-          label: "label"
+          children: "childrenNodes",
+          label: "labelName"
         },
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: ''
+        newPwd:"",
+        employeeList: [],
+        personBusinessId: ""
       };
     },
+    mounted: function () {
+      this.$nextTick(function () {
+        // 代码保证 this.$el 在 document 中
+        console.log(this);
+        //读取公司，部门数据
+        this.$api
+          .post({
+            url: "/sys/account/company/tree",
+            token: false
+          })
+          .then(e => {
+            console.log(e.data);
+            let result = e.data;
+            if (result.code == 200) {
+              console.log(result.message);
+              console.log(result.data);
+              this.treeData = result.data;
+            } else {
+              console.log("发送公告结果：" + result.message);
+              alert(result.message);
+            }
+          })
+          .catch(e => {
+            console.log("发送公告结果");
+            console.log(e);
+          });
+
+      })
+    },
     methods: {
-      checkcheck(e, data) {
-        console.log(e, data);
-        //  console.log(this.$refs.tree2.getNode(44), "getNode");
+      treeCheck(e, data) {
+        //判断如果未选中，则清空用户列表；
+        if(data && data.checkedKeys){
+          if(data.checkedKeys.length < 1){
+            this.employeeList=[];
+            return;
+          }
+        }
+        console.log(e, data,"check..");
+        //读取公司或部门下面的员工
+        this.$api
+          .post({
+            url:"/sys/account/employees/" + e.type +"/" +e.businessId,
+            token: false
+          })
+          .then(e => {
+            console.log(e.data);
+            let result = e.data;
+            if (result.code == 200) {
+              console.log(result.message);
+              console.log(result.data);
+              this.employeeList = result.data;
+            } else {
+              console.log("获取员工失败：" + result.message);
+              this.$message.error("获取员工失败：" + result.message);
+            }
+          })
+          .catch(e => {
+            console.log("获取员工异常");
+            console.log(e);
+            this.$message.error("获取员工异常" + e);
+          });
+         console.log(this.$refs.tree2.getNode(44), "getNode");
       },
       getCurrentKey() {
         console.log(this.$refs.tree2.getCurrentKey(), "getCurrentKey");
         //  this.$refs.tree2.setCurrentKey(44);
       },
       checkChange(e, data, childData) {
-        //console.log(e, data, childData, "checkChange");
-        //console.log(this.$refs.tree2.getCheckedKeys(), "id");
-        // console.log(this.$refs.tree2.getCheckedNodes());
-        //console.log(this.$refs.tree2.getNode(false, true));
+        console.log(e, data, childData, "checkChange");
       },
       //树输入筛选
       filterNode(value, data) {
+        console.log(value,data);
         if (!value) return true;
         return data.label.indexOf(value) !== -1;
       },
-      //树点击
-      handleNodeClick(e) {
-        console.log(e);
-        alert(e);
-      },
-      //筛选查询
-      queryTabData() {},
-      handleClick() {},
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
-      },
-      handleCurrentChange(val) {
-        console.log(`当前页: ${val}`);
-      },
-      handleSizeChange(val) {
-        console.log(`每1页 ${val} 条`);
+      //重置密码
+      resetPwd(){
+        //获取参数进行重置密码;
+        if(!this.personBusinessId || this.personBusinessId == "" || this.personBusinessId == null){
+          alert("请先选择人员");
+          return;
+        }
+        // let params = {accountId: this.personBusinessId};
+        // data: params,
+        //   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        this.$api
+          .post({
+            url:"/sys/account/reset/pwd/"+this.personBusinessId,
+            token: false
+          })
+          .then(e => {
+            console.log(e.data);
+            let result = e.data;
+            if (result.code == 200) {
+              console.log(result.message);
+              console.log(result.data);
+              this.newPwd = result.data;
+            } else {
+              console.log("重置密码失败：" + result.message);
+              this.$message.error("重置密码失败：" + result.message);
+            }
+          })
+          .catch(e => {
+            console.log("重置密码失败异常");
+            console.log(e);
+            this.$message.error("重置密码失败异常" + e);
+          });
       }
     }
   };
