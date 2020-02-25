e908f21e8487fd41870e8164b0514b066b2c75a5
lhp <lhp993356686@qq.com>
2020-02-25 17:37:27 +0800

    公司PC管理

--- src/components/asideNav.vue
+++ src/components/asideNav.vue
@@ -37,18 +37,17 @@
           <span>系统管理</span>
         </template>
 
-
         <el-menu-item index="/sys/noticeManageList">公告管理</el-menu-item>
         <el-menu-item index="/sys/onLineUserList">在线用户</el-menu-item>
         <el-menu-item index="/sys/systemConfigList">系统参数配置</el-menu-item>
         <el-menu-item index="/buySellSystem/agentHouseContrast">房源对比</el-menu-item>
         <el-menu-item index="/sys/passwordReset">用户密码重置</el-menu-item>
-        <el-menu-item index="/sys/pcOnline/list">PC在线管理</el-menu-item>
+        <el-menu-item index="/sys/pcManagement/list">公司PC管理</el-menu-item>
+        <el-menu-item index="/sys/pcOnline/list">公司PC在线管理</el-menu-item>
         <el-menu-item index="/sys/roleManagementList">岗位管理</el-menu-item>
         <el-menu-item index="/sys/companyList">公司管理</el-menu-item>
         <el-menu-item index="/sys/deptManageList">部门管理</el-menu-item>
         <el-menu-item index="/sys/employeeList">员工管理</el-menu-item>
-
       </el-submenu>
     </el-menu>
   </div>
