ff0e44df4d8d3d9e2bd09a106d96a0f174d58c2c
hkm <982485841@qq.com>
2020-02-25 18:21:09 +0800

    核心盘页面对接查询楼盘、添加关注接口

--- src/pages/buySellSystem/concernCommunity/concernCommunity.vue
+++ src/pages/buySellSystem/concernCommunity/concernCommunity.vue
@@ -10,61 +10,42 @@
              <div >
              <div style="margin-top:10px;height:30px;width:270px;background:RoyalBlue;font-weight:bold;font-size:20px;" >
                  <div style="color:white;float: left;margin-left:10px;">我的核心盘 </div>
-                 <div style="color:white;float: right;margin-right:10px;">{{this.countConcern}}套 </div>
+                 <div style="color:white;float: right;margin-right:10px;"> {{countAll}}套 </div>
              </div>
              <div style="height:30px;width:270px;background:DarkOrange;font-weight:bold;font-size:20px;">
                  <div style="color:white;float: left;margin-left:10px;">存量房总套数 </div>
-                 <div style="color:white;float: right;margin-right:10px;">1000套 </div>
+                 <div style="color:white;float: right;margin-right:10px;">{{countConcern}}套 </div>
              </div>
              <div style="height:30px;width:270px;background:LightCoral;font-weight:bold;font-size:20px;">
                  <div style="color:white;float: left;margin-left:10px;">在售总套数 </div>
-                 <div style="color:white;float: right;margin-right:10px;">100套 </div>
-             </div>
-              <div style="height:30px;width:270px;font-size:20px;">
-                 <el-input placeholder="输入您想添加的核心盘" style="margin-top:10px;width:270px;" ></el-input>
-             </div>
-             <div style="height:150px;margin-top:20px;width:270px;font-size:20px;border:1px solid #D3D3D3">
-                  <div style="color:black;margin-left:10px;">搜索结果 </div>
-                <div style="height:30px;width:270px;background:white;font-weight:bold;font-size:15px;margin-top:10px;">
-                  <div style="color:black;float: left;margin-left:10px;">五洲财富广场 </div>
-                  <div style="cursor:pointer;color:blue;float: right;margin-right:10px;" @click="addCommunity(1)">添加 </div>
-                </div>
-                <div style="height:30px;width:270px;background:white;font-weight:bold;font-size:15px;margin-top:10px;">
-                  <div style="color:black;float: left;margin-left:10px;">五洲财富广场 </div>
-                  <div style="cursor:pointer;color:blue;float: right;margin-right:10px;" @click="addCommunity(1)">添加 </div>
-                </div>
-                <div style="height:30px;width:270px;background:white;font-weight:bold;font-size:15px;margin-top:10px;">
-                  <div style="color:black;float: left;margin-left:10px;">五洲财富广场 </div>
-                  <div style="cursor:pointer;color:blue;float: right;margin-right:10px;" @click="addCommunity(1)">添加 </div>
-                </div>
-             </div>
-          </div>
-
-            <div style="height:40px;width:270px;background:CornflowerBlue;font-weight:bold;margin-top:10px;">
-                 <div style="color:white;float: left;margin-left:5px;font-size:20px;">建发上郡 </div>
-                  <div style="color:white;float: right;margin-right:15px;font-size:10px;">
-                     <div>存量套数</div>
-                     <div>1000套</div>
-                </div>
-                 <div style="color:white;float: right;margin-right:15px;font-size:10px;">
-                     <div>在售套数</div>
-                     <div>100套</div>
-                </div>
+                 <div style="color:white;float: right;margin-right:10px;">{{countEffectiveNum}}套 </div>
              </div>
              
-            
-             <div style="height:40px;width:270px;background:CornflowerBlue;font-weight:bold;margin-top:10px;">
-                 <div style="color:white;float: left;margin-left:5px;font-size:20px;">建发上郡 </div>
-                  <div style="color:white;float: right;margin-right:15px;font-size:10px;">
-                     <div>存量套数</div>
-                     <div>1000套</div>
-                </div>
-                 <div style="color:white;float: right;margin-right:15px;font-size:10px;">
-                     <div>在售套数</div>
-                     <div>100套</div>
+          </div>
+          <div style="height:30px;width:270px;font-size:20px;">
+                 <el-input @keyup.enter.native="queryNotConcernCommunityList()" placeholder="输入您想添加的核心盘" v-model="queryData.selectCommunity" style="margin-top:10px;width:270px"></el-input>
+             </div>
+             <div style="margin-top:20px;width:270px;font-size:20px;border:1px solid #D3D3D3" id="comList">
+                  <div style="color:black;margin-left:10px;">搜索结果 </div>
+                  <input style="display:none"  />
+                  <input style="display:none"/>
+                <div  v-for="(item,y) in list" :key="y"  style="height:30px;width:270px;background:white;font-weight:bold;font-size:15px;margin-top:10px;">
+                  <div style="color:black;float: left;margin-left:10px;">{{item.communityName}} </div>
+                  <div style="cursor:pointer;color:blue;float: right;margin-right:10px;" @click="addCommunity(item.id,item.communityName)">添加 </div>
                 </div>
              </div>
 
+            <div  v-for="(item,i) in array" :key="i" style="height:40px;width:270px;background:CornflowerBlue;font-weight:bold;margin-top:10px;">
+                 <div style="color:white;float: left;margin-left:5px;font-size:15px;">{{item.communityName}}</div>
+                  <div style="color:white;float: right;margin-right:15px;font-size:10px;">
+                     <div>存量套数</div>
+                     <div>{{item.countConcernCommunity}}套</div>
+                </div>
+                 <div style="color:white;float: right;margin-right:15px;font-size:10px;">
+                     <div>在售套数</div>
+                     <div>{{item.effectiveNum}}套</div>
+                </div>
+             </div>
 
           </div>
 
@@ -78,11 +59,11 @@
                <el-input placeholder="栋座" v-model="queryData.houseName" style="margin-left:10px;width:100px"></el-input>
                <el-input placeholder="房间号" v-model="queryData.houseName" style="margin-left:10px;width:100px"></el-input>
        
-       <el-input placeholder="姓名" style="margin-left:30px;width:240px" v-model="queryData.brokerPhone" clearable>
+       <el-input placeholder="姓名" style="margin-left:30px;width:240px" v-model="queryData.Customers" clearable>
         <template slot="prepend">业主</template>
         </el-input>
 
-        <el-input placeholder="业主电话" v-model="queryData.brokerPhone" style="margin-left:30px;width:240px" clearable>
+        <el-input placeholder="业主电话" v-model="queryData.Tel" style="margin-left:30px;width:240px" clearable>
         <template slot="prepend">电话</template>
         </el-input>
 
@@ -109,7 +90,7 @@
                      style="margin-left:30px"
                      size="mini"
                      @click="querylistByParams()">查询</el-button>
-        </div>
+        </div> 
     </template>
      
     <template #tableColumn="">
@@ -154,27 +135,7 @@
           {{scope.row.AddTime}}
         </template>
       </el-table-column>
-       <el-table-column label="跟单人">
-        <template v-slot="scope">
-          {{scope.row.agentPerName}}
-        </template>
-      </el-table-column>
-      <el-table-column prop="operation"
-                       label="操作"
-                       fixed="right"
-                       key={{scope.row.id}}>
-        <template v-slot="scope">
-          <!-- <div v-if="scope.row.operation!=''"> -->
-          <el-button type="info"
-                     size="mini"
-                     @click="distributeEvent(item.methosName,scope.row.id)"
-                     v-for="(item,index) in isForBut(2)"
-                     :key="index">{{item.name}}</el-button>
-          <!-- </div> -->
-          
-        </template>
-        
-      </el-table-column>
+      
     </template>
   </list-page>
 </template>
@@ -185,7 +146,9 @@ export default {
     listPage
   },
   data () {
+    
     return {
+      list:[1,2,3,4,5,6],
         input:'',
       loading: true, //控制表格加载动画提示
       pageJson: {
@@ -243,18 +206,38 @@ export default {
     querylistByParams () {
       this.querylist(1);
     },
-    addCommunity(comid){
-        console.log("参数");
-        console.log(comid);
+    addCommunity(id,name){
+        console.log("参数-----id:"+id+"---name："+name);
+        let params = { CommunityID: id+'', CommunityName: name+''};
+        this.$api.post({
+        url: '/concern_community/add',
+        headers: { "Content-Type": "application/json;charset=UTF-8" },
+       data: params,
+        token: false
+      }).then((e) => {
+        let result = e.data;
+        if (result.code == 200) {
+          alert(result.message);
+        } else {
+          console.log("添加关注" + result.message);
+          alert(result.message);
+        }
+      }).catch((e) => {
+         alert("添加关注失败");
+        console.log(e);
+      })
+
     },
     querylist (currentPage) {
       let params = { limit: this.pageJson.pageSize+'', page: currentPage+''};
       let that = this;
-      if (this.queryData.CommunityName != null) { params.CommunityName = this.queryData.CommunityName;}
-      if (this.queryData.minPrice != null) { params.minPrice = this.queryData.minPrice;}
-      if (this.queryData.maxPrice != null) { params.maxPrice = this.queryData.maxPrice;}
-      if (this.queryData.minInArea != null) { params.minInArea = this.queryData.minInArea;}
-      if (this.queryData.maxInArea != null) { params.maxInArea = this.queryData.maxInArea;}
+      if (this.queryData.CommunityName != null && this.queryData.CommunityName != '') { params.CommunityName = this.queryData.CommunityName;}
+      if (this.queryData.Customers != null && this.queryData.Customers != '') { params.Customers = this.queryData.Customers;}
+      if (this.queryData.Tel != null && this.queryData.Tel != '') { params.Tel = this.queryData.Tel;}
+      if (this.queryData.minPrice != null&& this.queryData.minPrice != '') { params.minPrice = this.queryData.minPrice;}
+      if (this.queryData.maxPrice != null&& this.queryData.maxPrice != '') { params.maxPrice = this.queryData.maxPrice;}
+      if (this.queryData.minInArea != null&& this.queryData.minInArea != '') { params.minInArea = this.queryData.minInArea;}
+      if (this.queryData.maxInArea != null&& this.queryData.maxInArea != '') { params.maxInArea = this.queryData.maxInArea;}
       this.$api.post({
         url: '/concern_community/list',
         headers: { "Content-Type": "application/json;charset=UTF-8" },
@@ -289,17 +272,24 @@ export default {
         if (result.code == 200) {
           console.log(result.message);
           console.log("统计结果"+result.data);
-          var that = this;
           var array = result.data;
           this.array = array;
-          that.array = array;
           var countConcern = 0;
-          for( var j=0;j < array.length;j++) {
-              console.log( "总数："+array[j].countConcernCommunity +"。楼盘id："+array[j].concernCommunityID+"。楼盘名称："+array[j].concernCommunityName);
+          var countEffectiveNum = 0;
+          var countAll=0;
+          for(var j=0;j <array.length;j++) {
               countConcern=countConcern+parseInt(array[j].countConcernCommunity) ;
+              countEffectiveNum = countEffectiveNum+parseInt(array[j].effectiveNum) ;
+              countAll++;
          } 
-         this.countConcern = countConcern;
-        console.log("总数"+countConcern);
+           this.countConcern = countConcern;
+           this.countAll = countAll;
+           this.countEffectiveNum=countEffectiveNum;
+          console.log("总数："+countConcern);
+           return array.forEach(item => {
+            return item.array;
+        });
+
         } else {
           console.log("查询核心盘统计结果then：" + result.message);
           alert(result.message);
@@ -309,6 +299,40 @@ export default {
         console.log(e);
       })
     },
+
+queryNotConcernCommunityList(){
+
+   var communityName = this.queryData.selectCommunity;
+   this.hasClassA
+      this.$api.post({
+        url: '/concern_community/notConcernCommunityList?CommunityName='+communityName,
+        token: false
+      }).then((e) => {
+        console.log(e.data);
+        let result = e.data;
+        
+        if (result.code == 200) {
+          console.log(result.message);
+          console.log("楼盘列表"+result.data);
+          var that = this;
+           var arrayCommunity = result.data;
+          this.list = arrayCommunity;
+           return list.forEach(item => {
+            return item.list;
+        });
+        alert(result.message);
+
+        } else {
+          console.log("查询未关注楼盘then：" + result.message);
+          
+        }
+      }).catch((e) => {
+        console.log("查询未关注楼盘失败catch");
+        console.log(e);
+      })
+    },
+
+
     distributeEvent (e, id) {
       this[e](id);
     },
@@ -317,6 +341,7 @@ export default {
         { name: '查看', isType: '1,2,3', methosName: '' }
       ]
       return array.filter((item) => {
+        this.item.push("12222222222222222222222222222222222")
         return item.isType.includes(type)
       })
     },
