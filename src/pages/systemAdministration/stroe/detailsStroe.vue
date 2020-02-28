3666e807524cdbc0cde6bc82e5f041347226051b
Administrator <hbr@qq.com>
2020-02-28 19:57:41 +0800

    店面详情列表页面

--- src/pages/systemAdministration/stroe/detailsStroe.vue
+++ src/pages/systemAdministration/stroe/detailsStroe.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div style="float:left;border:1px solid red">
-      <el-form ref="form" :v-model="form" label-width="120px">
-          <div style="count">
-              <el-form-item label="店面">
+    <div style="float:left;border:1px solid blue;text-align:center;padding : 0px 80px 0px 30px;">
+      <el-form ref="form" :v-model="form" label-width="120px" text-align:cente>
+        <!-- <h1 style="text-align :center;">店面信息</h1> -->
+        <el-form-item label="店面">
           <el-input
             v-model="form.storeName"
             :readonly="true"
@@ -52,17 +52,38 @@
             style="width:200px;border:0px solid  red"
           ></el-input>
         </el-form-item>
+        <!-- <div style="text-align:center;"> -->
+        <el-form-item>
+          <el-input
+            placeholder="小组名"
+            list="deptNameList"
+            v-model="queryData.keyWord"
+            style="width:300px;border:1px solid  red;margin:0px 20px 0px 0px"
+            @focus="findByParams()"
+            clearable
+          >
+            <template slot="prepend">要添加的小组</template>
+          </el-input>
+          <datalist id="deptNameList">
+            <option>关闭</option>
+            <option v-for="item in deptNameList" :key="item">{{item.deptName}}</option>
+          </datalist>
+        </el-form-item>
+
         <el-form-item>
           <el-button type="primary" @click="onSubmit">确定</el-button>
           <el-button @click="back">取消</el-button>
-        </el-form-item> 
-          </div>
-       
+        </el-form-item>
+        <!-- </div> -->
       </el-form>
     </div>
-    <list-page :parentData="$data" style="float:left;border:1px solid red"
-                @handleSizeChange="handleSizeChange"
-                @handleCurrentChange="handleCurrentChange">
+
+    <list-page
+      :parentData="$data"
+      style="float:left;border:1px solid red"
+      @handleSizeChange="handleSizeChange"
+      @handleCurrentChange="handleCurrentChange"
+    >
       <template v-slot:tableColumn="cell">
         <template v-for="item in cell.tableData">
           <el-table-column
@@ -94,14 +115,26 @@
 <script>
 import listPage from "@/components/listPage";
 export default {
-    components:{
-        listPage
-    },
+  components: {
+    listPage
+  },
   data() {
     return {
       loading: false,
+      stroeDeptEntity: {
+        sId: null,
+        deptID: null,
+        memo: null
+      },
+      queryData: {
+        keyWord: "",
+        coId: 0,
+        pId: -2,
+        isLocked: 1
+      },
+      deptNameList: [],
       tableDataColumn: [{ prop: "deptName", label: "店内小组" }],
-      tableData: [{deptName:null,id:null}],
+      tableData: [{ deptName: null, id: null }],
       form: {
         id: null,
         storeName: null,
@@ -111,79 +144,131 @@ export default {
         shoreType: null,
         storeDesc: null,
         regDate: null
+      },
+      pageJson: {
+        currentPage: 1, //当前页码
+        total: 10, //总记录数
+        pageSize: 10 //每页条数
       }
     };
   },
   created() {
     this.id = this.$route.params.id;
+    
   },
   mounted() {
-    this.$api
-      .post({
-        url: "/stroe/" + this.$route.params.id,
-        token: false
-      })
-      .then(e => {
-        console.log(e.data);
-        let result = e.data;
-        if (result.code == 200) {
-          console.log(result.message);
-          console.log(result.data);
-          for (var i = 0; i < result.data.length; i++) {
-            switch (result.data[i].flagSale) {
-              case "0":
-                result.data[i].flagSale = "文职";
-                break;
-              case "1":
-                result.data[i].flagSale = "职务";
-                break;
-            }
-            switch (result.data[i].shoreType) {
-              case 0:
-                result.data[i].shoreType = "加盟店";
-                break;
-              case 1:
-                result.data[i].shoreType = "直营店";
-                break;
-            }
-          }
-          this.form = result.data;
-        } else {
-          console.log("查询实体店详情结果：" + result.message);
-          alert(result.message);
-        }
-      })
-      .catch(e => {
-        console.log("查询实体店详情失败");
-        console.log(e);
-      });
-//获取实体店下小组列表
-    this.$api
-      .get({
-        url: "/stroe/" + this.$route.params.id,
-        token: false
-      })
-      .then(e => {
-        console.log(e.data);
-        let result = e.data;
-        if (result.code == 200) {
-          console.log(result.message);
-          console.log(result.data);
-          
-          this.tableData = result.data.list;
-        } else {
-          console.log("查询实体店详情结果：" + result.message);
-          alert(result.message);
-        }
-      })
-      .catch(e => {
-        console.log("查询实体店详情失败");
-        console.log(e);
-      });
+    this.list();
+    this.deptList(1);
   },
   methods: {
+    deptList(currentPage) {
+      //获取实体店下小组列表
+      let params = {
+        limit: this.pageJson.pageSize,
+        page: currentPage,
+        id: this.$route.params.id
+      };
+      this.$api
+        .post({
+          url: "/stroe/byId ",
+          data: params,
+          token: false,
+          headers: { "Content-Type": "application/json" }
+        })
+        .then(e => {
+          console.log(e.data);
+          let result = e.data;
+          if (result.code == 200) {
+            console.log(result.message);
+            console.log(result.data);
+            this.pageJson.total = result.data.totalCount;
+            this.pageJson.currentPage = result.data.currPage;
+            this.tableData = result.data.list;
+          } else {
+            console.log("查询实体店详情结果：" + result.message);
+            alert(result.message);
+          }
+        })
+        .catch(e => {
+          console.log("查询实体店详情失败");
+          console.log(e);
+        });
+    },
+    list() {
+      this.$api
+        .post({
+          url: "/stroe/" + this.$route.params.id,
+          token: false
+        })
+        .then(e => {
+          console.log(e.data);
+          let result = e.data;
+          if (result.code == 200) {
+            console.log(result.message);
+            console.log(result.data);
+            for (var i = 0; i < result.data.length; i++) {
+              switch (result.data[i].flagSale) {
+                case "0":
+                  result.data[i].flagSale = "文职";
+                  break;
+                case "1":
+                  result.data[i].flagSale = "职务";
+                  break;
+              }
+              switch (result.data[i].shoreType) {
+                case 0:
+                  result.data[i].shoreType = "加盟店";
+                  break;
+                case 1:
+                  result.data[i].shoreType = "直营店";
+                  break;
+              }
+            }
+            this.form = result.data;
+          } else {
+            console.log("查询实体店详情结果：" + result.message);
+            alert(result.message);
+          }
+        })
+        .catch(e => {
+          console.log("查询实体店详情失败");
+          console.log(e);
+        });
+    },
     onSubmit() {
-      console.log("submit!");
+    
+      console.log(this.queryData.keyWord);
+      this.deptNameList.forEach(item => {
+        if (item.deptName.indexOf(this.queryData.keyWord) != -1) {
+          this.stroeDeptEntity.sId = this.id;
+          this.stroeDeptEntity.deptID = item.deptId;
+        }
+      });
+      console.log("this", this.stroeDeptEntity);
+
+      this.$api
+        .post({
+          url: "/stroe/deptName/add",
+          data: this.stroeDeptEntity,
+          token: false,
+          headers: { "Content-Type": "application/json;charset=UTF-8" }
+        })
+        .then(e => {
+          let result = e.data;
+          if (result.code == 200) {
+            console.log(result.message);
+            this.$alert("", "添加成功", {
+              dangerouslyUseHTMLString: false
+            });
+            this.$router.push({ path: "/sys/stroeList" });
+            console.log(result.data);
+            this.$message({ message: result.message });
+          }
+        })
+        .catch(e => {
+          console.log("添加失败");
+          console.log(e);
+        });
     },
     back() {
       this.$router.push({ path: "/sys/stroeList" });
@@ -191,18 +276,68 @@ export default {
     distributeEvent(e, id) {
       this[e](id);
     },
+    delDeptDetail(id){
+      debugger;
+      let params ={ id:this.id,
+                    deptId:id};
+      this.$api.post({
+        url: '/stroe/deptName/del',
+        data:params,
+        //token: false,
+        qs:true,
+        headers: { "Content-Type": "application/json;charset=UTF-8" }
+      }).then((e) => {
+        let result = e.data;
+        if (result.code == 200) {
+          this.$alert('', '删除成功', {
+            dangerouslyUseHTMLString: false
+          });
+          this.$router.push({ path: "/sys/stroeList"});
+        }
+      }).catch((e) => {
+        console.log("删除失败");
+        console.log(e);
+      })
+    },
     getOpeBtns(type) {
       let array = [{ name: "删除", isType: "1", methosName: "delDeptDetail" }];
       return array;
     },
-    handleSizeChange (val) {
+    findByParams() {
+      let params = this.queryData;
+      params.id = this.id;
+      if (this.queryData.keyWord != null && this.queryData.keyWord != "") {
+        params.keyWord = this.queryData.keyWord;
+      }
+      this.$api
+        .post({
+          url: "/stroe/deptName",
+          data: params,
+         token: false,
+        })
+        .then(e => {
+          let result = e.data;
+          if (result.code == 200) {
+            debugger;
+            this.deptNameList = result.data;
+            console.log(this.deptNameList);
+          } else {
+            console.log("查询实体店详情结果：" + result.message);
+            alert(result.message);
+          }
+        })
+        .catch(e => {
+          console.log("查询实体店详情失败");
+        });
+    },
+    handleSizeChange(val) {
       console.log(`设置了每页 ${val} 条`);
       this.pageJson.pageSize = val;
       this.queryDeptDatas(1);
     },
-    handleCurrentChange (val) {
-    
-    },
+    handleCurrentChange(val) {
+      this.deptList(val);
+    }
   }
 };
 </script>
