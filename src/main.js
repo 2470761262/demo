b899c66e92b59d44ee8ad4384c5447a84a41dac7
XL <1598675282@qq.com>
2020-03-05 14:59:14 +0800

    Signed-off-by: XL <1598675282@qq.com>

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import store from "@/store/store";
+import VueCookies from 'vue-cookies';//cookies
 import "./validate/validate"; //表单验证
 import Api from "@/api/require"; //请求api
 import guard from "@/router/guard"; // 路由拦截器
@@ -16,6 +17,7 @@ Vue.prototype.socketApi = socketApi
 Vue.prototype.$api = Api;
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
+Vue.use(VueCookies);
 guard(router);
 themePackers.createImport();
 /* eslint-disable no-new */
