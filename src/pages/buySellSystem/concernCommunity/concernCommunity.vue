7cba0891b0503d6809ea284ffac0b64107876123
csc <csc@qq.com>
2020-03-18 09:26:09 +0800

    提交

--- src/pages/buySellSystem/concernCommunity/concernCommunity.vue
+++ src/pages/buySellSystem/concernCommunity/concernCommunity.vue
@@ -1,8 +1,3 @@
-<style>
-.div {
-  position: absolute;
-}
-</style>
 <template>
   <list-page :parentData="$data"
              @sort-change="sortMethod"
@@ -77,8 +72,7 @@
 
       <div class="page-inline budingMarinSet">
 
-        <el-item label="楼盘名称"
-                 prop="comId">
+        
           <el-select v-model="comId"
                      @focus="remoteInput"
                      @change="queryCBId()"
@@ -94,10 +88,7 @@
                        :value="item.value">
             </el-option>
           </el-select>
-        </el-item>
-        <el-item label="栋座"
-                 prop="cbId"
-                 class="page-label-center">
+       
           <el-select v-model="cbId"
                      filterable
                      clearable
@@ -109,11 +100,7 @@
                        :value="item.value">
             </el-option>
           </el-select>
-        </el-item>
-        <el-item label="房间号"
-                 prop="roomNo"
-                 clearable
-                 class="page-label-center">
+       
           <el-select v-model="roomNo"
                      filterable
                      placeholder="请选择房间号">
@@ -123,7 +110,7 @@
                        :value="item.value">
             </el-option>
           </el-select>
-        </el-item>
+        
         <span>
           房源状态
         </span>
@@ -136,13 +123,11 @@
           </el-option>
         </el-select>
         <span>
-          <input type="checkbox"
-                 style='margin-left:10px'
-                 @click="keySelect()" /> 钥匙</span>
+          <input type="checkbox" style='margin-left:10px'
+                       @click="keySelect" /> 钥匙</span>
         <span>
-          <input type="checkbox"
-                 style='margin-left:10px;background:#fff'
-                 @click="onlySelect()" /> 独家</span>
+           <input type="checkbox" style='margin-left:10px;background:#fff'
+                       @click="onlySelect" /> 独家</span>
       </div>
       <div style="width:1000px">
         <el-input placeholder="姓名"
@@ -181,14 +166,14 @@
                   style="margin-left:3px;width:100px"></el-input>
       </div>
       <el-button type="primary"
-                 style="height:40px;margin-right:5px;margin-top:10px"
+                 style="margin-left:30px"
                  size="mini"
                  @click="querylistByParams()">查询</el-button>
-      <moreSelect @moreSlectChange="moreSlectChange"
-                  style="height:40px;margin-right:5px;margin-top:10px"></moreSelect>
-      <div class="div">
+      <el-button type="primary"
+                 style="margin-left:30px"
+                 size="mini"
+                 @click="querylistByParams()">更多筛选</el-button>
 
-      </div>
     </template>
 
     <template #tableColumn="">
@@ -279,14 +264,12 @@
 </template>
 <script>
 import listPage from '@/components/listPage';
-import moreSelect from '@/components/moreSelect';
 import getMenuRid from '@/minxi/getMenuRid';
 import houseContrast from '@/minxi/houseContrast';
 export default {
   mixins: [getMenuRid, houseContrast],
   components: {
-    listPage,
-    moreSelect
+    listPage
   },
   data () {
 
@@ -306,21 +289,20 @@ export default {
         { value: '3', label: "房源编号3" },
       ],
       tableDataColumn: [
-        { prop: 'HouseNo', label: "房源编号", width: '110px', order: false, disabled: false, default: true },
-        { prop: 'CommunityName', label: "楼盘名称", width: '110px', order: false, disabled: false, default: true },
-        { prop: 'price', label: "售价(万元)", width: '120px', order: 'custom', disabled: false, default: true, formart: item => item.Price + '万元' },
-        { prop: 'inArea', label: "面积(m²)", width: '110px', order: 'custom', disabled: false, default: true, formart: item => item.InArea + 'm²' },
+        { prop: 'houseNo', label: "房源编号", width: '110px', order: false, disabled: false, default: true },
+        { prop: 'communityName', label: "楼盘名称", width: '110px', order: false, disabled: false, default: true },
+        { prop: 'price', label: "售价(万元)", width: '120px', order: 'custom', disabled: false, default: true, formart: item => item.price + '万元' },
+        { prop: 'inArea', label: "面积(m²)", width: '110px', order: 'custom', disabled: false, default: true, formart: item => item.inArea + 'm²' },
         { prop: 'unitpaice', label: "均价(元/平)", width: '130px', order: 'custom', disabled: false, default: true, format: item => item.unitpaice + '元/㎡' },
         { prop: 'decoration', label: "装修程度", width: '110px', order: false, disabled: false, default: true },
         { prop: 'seenNum', label: '被看次数', width: '120', order: 'custom', disabled: false, default: true },
         { prop: 'outfollow', label: '未跟进天数', width: '120', order: 'custom', disabled: false, default: true },
         { prop: 'notLookNum', label: '未被看天数', width: '120', order: 'custom', disabled: false, default: true },
-        // { prop: '', label: "房源状态", width: '110px', order: false, disabled: false, default: true },//自己补充
+       // { prop: '', label: "房源状态", width: '110px', order: false, disabled: false, default: true },//自己补充
         { prop: 'agentPerName', label: "跟单人", width: '110px', order: false, disabled: false, default: true },
-        { prop: '', label: "户型", width: '110px', order: false, disabled: false, default: true, formart: item => item.Rooms + '室' + item.hall + '厅' + item.toilet + '卫' },
+        { prop: '', label: "户型", width: '110px', order: false, disabled: false, default: true, formart: item => item.rooms + '室' + item.hall + '厅' + item.toilet + '卫' },
       ],
-      tableData: {},
-      moreSlect: [],
+      tableData: [],
       elTabs: {
         activeName: "tab1",
         list: [
@@ -340,6 +322,7 @@ export default {
       roomNo: '',
       queryData: {
         communityName: '',
+        selectCommunity:'',
         isOnly: '',
         keyOwner: '',
       }
@@ -354,12 +337,7 @@ export default {
   methods: {
     sortMethod (e) {
       console.log(e, "eeee排序");
-      this.queryVerifyHouseDatas(1, 'id', 'ascending');
-    },
-    moreSlectChange (e) {
-
-      this.moreSlect = e;
-      console.log(Object.keys(this.moreSlect).length);
+      this.queryVerifyHouseDatas(1, e.prop, e.order);
     },
     keySelect () {
       if (this.queryData.keyOwner != '') {
@@ -367,6 +345,7 @@ export default {
       } else {
         this.queryData.keyOwner = '1';
       }
+       this.queryVerifyHouseDatas(1, 'id', 'ascending');
     },
     onlySelect () {
       if (this.queryData.isOnly != '') {
@@ -374,7 +353,7 @@ export default {
       } else {
         this.queryData.isOnly = '1';
       }
-
+     this.queryVerifyHouseDatas(1, 'id', 'ascending');
     },
     selectedCommunity (e) {
       this.$confirm('是否确定关注该楼盘?', '提示', {
@@ -463,36 +442,36 @@ export default {
       })
     },
     queryVerifyHouseDatas (currentPage, column, type) {
-      let params = { limit: this.pageJson.pageSize + '', page: currentPage + '' };
+      let params = { limit: this.pageJson.pageSize, page: currentPage-1};
       let that = this;
-      if (Object.keys(this.moreSlect).length != 0) {
-        for (let key in this.moreSlect) {
-          if (this.key == 'addTime' && this.moreSlect[key] !== '') {
-            params.biginTime = this.moreSlect[key][0];
-            params.endTime = this.moreSlect[key][1];
-          } else {
-            params[key] = this.moreSlect[key]
-          }
-        }
+
+      if (this.comId != null && this.comId != '') { params.Comid = this.comId; }
+      if (this.cbId != null && this.cbId != '') { params.CBid = this.cbId; }
+      if (this.queryData.isOnly != null && this.queryData.isOnly != '') { params.isOnly = this.queryData.isOnly; }
+      if (this.queryData.keyOwner != null && this.queryData.keyOwner != '') { params.keyOwner = this.queryData.keyOwner; }
+      if (this.roomNo != null && this.roomNo != '') { params.BHID = this.roomNo; }
+      if (this.queryData.Customers != null && this.queryData.Customers != '') { params.Customers = this.queryData.Customers; }
+      if (this.queryData.Tel != null && this.queryData.Tel != '') { params.Tel = this.queryData.Tel; }
+      if (this.queryData.minPrice != null && this.queryData.minPrice != '') { params.minPrice = this.queryData.minPrice; }
+      if (this.queryData.maxPrice != null && this.queryData.maxPrice != '') { params.maxPrice = this.queryData.maxPrice; }
+      if (this.queryData.minInArea != null && this.queryData.minInArea != '') { params.minInArea = this.queryData.minInArea; }
+      if (this.queryData.maxInArea != null && this.queryData.maxInArea != '') { params.maxInArea = this.queryData.maxInArea; }
+      params.isOnly=that.queryData.isOnly;
+      params.keyOwner=that.queryData.keyOwner;
+       if (column == '' || type == null || type == undefined) {
+        params.sortColumn = 'id';
+      } else {
+        params.sortColumn = column;
       }
-      else {
-        if (this.comId != null && this.comId != '') { params.Comid = this.comId; }
-        if (this.cbId != null && this.cbId != '') { params.CBid = this.cbId; }
-        if (this.queryData.isOnly != null && this.queryData.isOnly != '') { params.isOnly = this.queryData.isOnly; }
-        if (this.queryData.keyOwner != null && this.queryData.keyOwner != '') { params.keyOwner = this.queryData.keyOwner; }
-        if (this.roomNo != null && this.roomNo != '') { params.BHID = this.roomNo; }
-        if (this.queryData.Customers != null && this.queryData.Customers != '') { params.Customers = this.queryData.Customers; }
-        if (this.queryData.Tel != null && this.queryData.Tel != '') { params.Tel = this.queryData.Tel; }
-        if (this.queryData.minPrice != null && this.queryData.minPrice != '') { params.minPrice = this.queryData.minPrice; }
-        if (this.queryData.maxPrice != null && this.queryData.maxPrice != '') { params.maxPrice = this.queryData.maxPrice; }
-        if (this.queryData.minInArea != null && this.queryData.minInArea != '') { params.minInArea = this.queryData.minInArea; }
-        if (this.queryData.maxInArea != null && this.queryData.maxInArea != '') { params.maxInArea = this.queryData.maxInArea; }
+      if (type == '' || type == null || type == undefined) {
+        params.sortType = 'ascending';
+      } else {
+        params.sortType = type;
       }
       this.$api.post({
-        url: '/concern_community/list',
-        headers: { "Content-Type": "application/json;charset=UTF-8" },
+        url: '/myHouse/getMyKernelHouse',
         data: params,
-        token: false
+        qs: true
       }).then((e) => {
         console.log(e.data);
         let result = e.data;
@@ -500,9 +479,8 @@ export default {
         if (result.code == 200) {
           console.log(result.message);
           console.log(result.data);
-          that.pageJson.total = result.data.totalCount;
-          that.pageJson.currentPage = result.data.currPage;
-          that.tableData = result.data.list;
+          that.pageJson.total = result.data.dataCount;
+          that.tableData = result.data.data;
         } else {
           console.log("查询我的核心盘列表结果：" + result.message);
           alert(result.message);
@@ -514,22 +492,19 @@ export default {
     },
     queryConcernCount () {
       this.$api.post({
-        url: '/concern_community/CommunityCount',
-        token: false
-      }).then((e) => {
-        console.log(e.data);
+        url: '/myHouse/getCommunityNum',
+        qs: true
+      }).then((e) => { 
         let result = e.data;
         if (result.code == 200) {
-          console.log(result.message);
-          console.log("统计结果" + result.data);
-          var array = result.data;
-          this.array = array;
+          this.array = result.data.data;
           var countConcern = 0;
           var countEffectiveNum = 0;
           var countAll = 0;
-          for (var j = 0; j < array.length; j++) {
-            countConcern = countConcern + parseInt(array[j].countConcernCommunity);
-            countEffectiveNum = countEffectiveNum + parseInt(array[j].effectiveNum);
+          for (var j = 0; j < this.array.length; j++) {
+            countConcern = countConcern + parseInt(this.array[j].countConcernCommunity);
+            console.log(parseInt(this.array[j].countConcernCommunity)+"                    =====================")
+            countEffectiveNum = countEffectiveNum + parseInt(this.array[j].effectiveNum);
             countAll++;
           }
           this.countConcern = countConcern;
@@ -554,20 +529,16 @@ export default {
       // if(communityName!=null&&communityName!=''&&communityName!='undefined'){
       this.hasClassA
       this.$api.post({
-        url: '/concern_community/notConcernCommunityList?CommunityName=' + communityName,
-        token: false
+        url: '/concern_community/notConcernCommunityList',
+        data:{CommunityName:communityName},
+        qs: true
       }).then((e) => {
         console.log(e.data);
         let result = e.data;
         if (result.code == 200) {
-          console.log(result.message);
-          console.log("楼盘列表" + result.data);
           var that = this;
-          var arrayCommunity = result.data;
-          this.list = arrayCommunity;
-          return list.forEach(item => {
-            return item.list;
-          });
+          that.list = result.data;
+         
         } else {
           console.log("查询未关注楼盘then：" + result.message);
         }
