972c6dac6e386f778a4f6b5f7739cc581d209810
XL <1598675282@qq.com>
2020-03-20 09:36:19 +0800

    冲突

--- src/pages/buySellSystem/houseRecommend/chiefRecommendHouse.vue
+++ src/pages/buySellSystem/houseRecommend/chiefRecommendHouse.vue
@@ -122,7 +122,6 @@ export default {
         pageSize: 10 //每页条数
       },
       tableColumnField: [
-<<<<<<< HEAD
         { prop: 'houseNo', label: '房源编号', width: '170', order: false, disabled: true, default: true },
         { prop: 'communityName', label: '小区名称', order: false, width: '150', disabled: true, default: true },
         { prop: 'buildingName', label: '楼栋号', width: '90', order: false, disabled: true, default: true },
@@ -131,18 +130,7 @@ export default {
         { prop: 'price', label: '售价(万元)', width: '120', order: 'custom', disabled: false, default: true, formart: item => item.price + '万元' },
         { prop: 'seenNum', label: '被看次数', width: '120', order: false, disabled: false, default: true },
         { prop: 'outfollow', label: '未跟进天数', width: '120', order: false, disabled: false, default: true },
-        { prop: 'notLookNum', label: '未被看天数', width: '120', order: false, disabled: false, default: true },
-=======
-        { prop: 'houseNo', label: '房源编号', width: '170', order: false, disabled: true, default: true},
-        { prop: 'communityName', label: '小区名称', order: false, width: '150', disabled: true, default: true},
-        { prop: 'buildingName', label: '楼栋号', width: '90', order: false, disabled: true, default: true},
-        { prop: 'roomNo', label: '房间号', width: '110', order: false, disabled: true, default: true},
-        { prop: 'inArea', label: '面积(m²)', width: '110', order: 'custom', disabled: false, default: true,formart: item=> item.inArea+'m²' },
-        { prop: 'price', label: '售价(万元)', width: '120', order: 'custom', disabled: false, default: true,formart: item=> item.price+'万元' },
-        { prop: 'seenNum', label: '被看次数', width: '120', order: false, disabled: false, default: true},
-        { prop: 'outfollow', label: '未跟进天数', width: '120', order: false, disabled: false, default: true},
-        { prop: 'noSeenDay', label: '未被看天数', width: '120', order: false, disabled: false, default: true},
->>>>>>> 8a6c01fd38db7a0088f1a4baa0b1a1c4498c93cb
+        { prop: 'noSeenDay', label: '未被看天数', width: '120', order: false, disabled: false, default: true },
         { prop: 'addTime', label: '添加时间', width: '120', order: false, disabled: false, default: true },
         { prop: 'brokerName', label: '跟单人', width: '120', order: false, disabled: false, default: true },
         { prop: 'houseType', label: '户型', width: '150', order: false, disabled: false, default: true, formart: item => item.rooms + '室' + item.hall + '厅' + item.toilet + '卫' },
