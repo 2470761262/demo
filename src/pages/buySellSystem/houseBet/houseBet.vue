377111921c4ade53ff82ed705b30cbb53c9db752
hxf <hxf@qq.com>
2020-03-20 17:33:05 +0800

    es

--- src/pages/buySellSystem/houseBet/houseBet.vue
+++ src/pages/buySellSystem/houseBet/houseBet.vue
@@ -12,10 +12,8 @@
                  :model="data"
                  ref="data"
                  size="medium">
-
           <el-form-item label="楼盘"
                         prop="customerName">
-
             <el-select v-model="data.comId"
                        @change="queryCBId()"
                        filterable
@@ -29,8 +27,7 @@
               <el-option v-for="item in options"
                          :key="item.value"
                          :label="item.name"
-                         :value="item.value">
-              </el-option>
+                         :value="item.value"></el-option>
             </el-select>
 
             <el-select v-model="data.cbId"
@@ -42,8 +39,7 @@
               <el-option v-for="item in cbIdList"
                          :key="item.value"
                          :label="item.name"
-                         :value="item.value">
-              </el-option>
+                         :value="item.value"></el-option>
             </el-select>
             <el-select v-model="data.roomNo"
                        filterable
@@ -53,8 +49,7 @@
               <el-option v-for="item in roomNoList"
                          :key="item.value"
                          :label="item.name"
-                         :value="item.value">
-              </el-option>
+                         :value="item.value"></el-option>
             </el-select>
           </el-form-item>
 
@@ -102,8 +97,7 @@
                             value-format="yyyy-MM-dd"
                             range-separator="至"
                             start-placeholder="起"
-                            end-placeholder="止">
-            </el-date-picker>
+                            end-placeholder="止"></el-date-picker>
           </el-form-item>
 
           <el-form-item label="对赌结果"
@@ -115,8 +109,7 @@
               <el-option v-for="item in betStatusList"
                          :key="item.value"
                          :label="item.name"
-                         :value="item.value">
-              </el-option>
+                         :value="item.value"></el-option>
             </el-select>
           </el-form-item>
 
@@ -147,15 +140,12 @@
                        :default-checked-keys="curNodeId"></el-tree>
             </div>
           </el-form-item>
-          <span style='color:rgb(90,159,203);cursor:pointer;margin-left:20px'
-                @click="Remove">
-            清除
-          </span>
+          <span style="color:rgb(90,159,203);cursor:pointer;margin-left:20px"
+                @click="Remove">清除</span>
           <el-button type="primary"
                      style="margin-left:10px"
                      size="mini"
-                     @click="queryHouseBetParams">查询
-          </el-button>
+                     @click="queryHouseBetParams">查询</el-button>
           <el-button style="margin-left:30px;width:50px;height:30px;border:0"
                      size="mini">
             <moreSelect @moreSelectChange="moreSelectChange"
@@ -170,8 +160,7 @@
         <el-table-column :prop="item.prop"
                          :label="item.label"
                          :width="item.width"
-                         :key="item.prop">
-        </el-table-column>
+                         :key="item.prop"></el-table-column>
       </template>
       <!--      房源编号、楼盘名称、售价（可排序）、面积（可排序）、单价（可排序）、户型（X室X厅X卫）、对赌鑫币值、预期奖励鑫币值、对赌时间（对赌成功当日）、对赌结果、剩余天数、对赌人、操作（查看）-->
       <el-table-column prop="price"
@@ -203,42 +192,37 @@
                        label="对赌时间"
                        :sort-orders="['ascending', 'descending']"
                        sortable="custom">
-<!--                       :render-header="customFieldColumn"-->
+        <!--                       :render-header="customFieldColumn"-->
       </el-table-column>
       <el-table-column prop="status"
                        label="对赌结果"
                        :formatter="formatHouseBetStatus">
       </el-table-column>
       <el-table-column prop="brokerName"
-                       label="对赌人">
-      </el-table-column>
+                       label="对赌人"></el-table-column>
       <el-table-column prop="endTime"
-                       label="到期时间">
-      </el-table-column>
+                       label="到期时间"></el-table-column>
       <el-table-column label="操作"
                        fixed="right"
                        key="operation">
         <template v-slot="scope">
           <el-button type="info"
                      size="mini"
-                     @click="toLook(scope.row)">查看
-          </el-button>
+                     @click="toLook(scope.row)">查看</el-button>
         </template>
       </el-table-column>
-
     </template>
   </list-page>
 </template>
 <script>
-import listPage from '@/components/listPage';
-import moreSelect from '@/components/moreSelect';
+import listPage from "@/components/listPage";
+import moreSelect from "@/components/moreSelect";
 export default {
   components: {
     listPage,
-    moreSelect,
+    moreSelect
   },
   data () {
-
     return {
       loading: true,
       showHrTree: false,
@@ -250,30 +234,30 @@ export default {
       treeData: [],
       curNodeId: [],
       data: {
-        comId: '',
-        cbId: '',
-        roomNo: '',
-        tel: '',
-        customerName: '',
-        status: '',
-        minMoney: '',
-        maxMoney: '',
-        order: '',
-        empId: '',
-        deptId: '',
-        coId: '',
-        empName: '',
-        orderAsc: '',
-        timeSelect: ''
+        comId: "",
+        cbId: "",
+        roomNo: "",
+        tel: "",
+        customerName: "",
+        status: "",
+        minMoney: "",
+        maxMoney: "",
+        order: "",
+        empId: "",
+        deptId: "",
+        coId: "",
+        empName: "",
+        orderAsc: "",
+        timeSelect: ""
       },
       options: [],
       cbIdList: [],
       roomNoList: [],
       betStatusList: [
-        { value: 0, name: '努力中' },
-        { value: 1, name: '成功' },
-        { value: 2, name: '失败' },
-        { value: 3, name: '过期' },
+        { value: 0, name: "努力中" },
+        { value: 1, name: "成功" },
+        { value: 2, name: "失败" },
+        { value: 3, name: "过期" }
       ],
       pageJson: {
         currentPage: 1, //当前页码
@@ -281,27 +265,27 @@ export default {
         pageSize: 10 //每页条数
       },
       tableDataColumn: [
-        { prop: 'houseNo', label: "房源编号" },
-        { prop: 'communityName', label: "楼盘名称" },
+        { prop: "houseNo", label: "房源编号" },
+        { prop: "communityName", label: "楼盘名称" }
       ],
-      tableData: [{
-        // house: '龙腾花园-16栋-604室',
-        // priceArea: '234万/100平',
-        // type: '3室2厅1卫',
-        // levae: '精装修',
-        // economicPro: '周杰伦',
-
-        // validateType: '通过',
-        // cutPro: '周杰伦1',
-        // addTime: '2019-01-01 18:00:00',
-        // cellType: '号码异常',
-        // operation: '3',
-      }],
-    }
+      tableData: [
+        {
+          // house: '龙腾花园-16栋-604室',
+          // priceArea: '234万/100平',
+          // type: '3室2厅1卫',
+          // levae: '精装修',
+          // economicPro: '周杰伦',
+          // validateType: '通过',
+          // cutPro: '周杰伦1',
+          // addTime: '2019-01-01 18:00:00',
+          // cellType: '号码异常',
+          // operation: '3',
+        }
+      ]
+    };
   },
   mounted () {
-
-    this.queryHouseBet(1, 'id', 'ascending');
+    this.queryHouseBet(1, "id", "ascending");
     //读取树数据
     this.$api
       .post({
@@ -337,18 +321,17 @@ export default {
   },
   methods: {
     moreSelectChange (e) {
-      debugger
-      if (e != '')
-
-        this.moreSelect = e;
-      this.queryHouseBet(1, 'id', 'ascending')
+      if (e != "") this.moreSelect = e;
+      this.queryHouseBet(1, "id", "ascending");
     },
     sortMethod (e) {
       console.log(e, "eeee排序");
       this.queryHouseBet(1, e.prop, e.order);
     },
     Remove () {
+      let tab = this.tableColumn;
       Object.assign(this.$data, this.$options.data.call(this));
+      this.tabColumnChange(tab);
       this.queryHouseBet(1, 'id', 'ascending');
 
     },
@@ -378,34 +361,34 @@ export default {
       return data.label.indexOf(value) !== -1;
     },
     queryTabData () {
-      console.log(this, '111');
+      console.log(this, "111");
     },
     formatHouseType (row, column) {
       if (row.rooms) {
-        return row.rooms + '室' + row.hall + '厅' + row.toilet + '卫';
+        return row.rooms + "室" + row.hall + "厅" + row.toilet + "卫";
       }
     },
     unitPrice (row, column) {
       if (row.rooms) {
-        return Math.round(row.price * 1000 / row.inArea);
+        return Math.round((row.price * 1000) / row.inArea);
       }
     },
     formatHouseBetStatus (row, column) {
       switch (row.status) {
         case 0:
-          return "努力中"
+          return "努力中";
           break;
         case 1:
-          return "成功"
+          return "成功";
           break;
         case 2:
-          return "失败"
+          return "失败";
           break;
         case 3:
-          return "过期"
+          return "过期";
           break;
         default:
-          return ""
+          return "";
           break;
       }
     },
@@ -426,10 +409,13 @@ export default {
     },
     toLook (row) {
       var that = this;
-      that.$router.push({ name: 'houseDetails', params: { "houseId": row.houseId } });
+      that.$router.push({
+        name: "houseDetails",
+        params: { houseId: row.houseId }
+      });
     },
     queryHouseBetParams () {
-      this.queryHouseBet(1, 'id', 'ascending');
+      this.queryHouseBet(1, "id", "ascending");
     },
     //楼盘获取焦点 第一次点击就进行查询
 
@@ -439,92 +425,96 @@ export default {
       }
     },
     remoteMethod (query) {
-      var that = this
-      if (query !== '') {
+      var that = this;
+      if (query !== "") {
         that.loading = true;
 
-        that.$api.get({
-          url: "/mateHouse/queryCommunity",
-          headers: { "Content-Type": "application/json;charset=UTF-8" },
-          token: false,
-          qs: true,
-          data: {
-            communityName: query
-          }
-        }).then((e) => {
-          console.log(e.data)
-          if (e.data.code == 200) {
-            that.loading = false;
-            that.options = e.data.data.list;
-
-          }
-        })
+        that.$api
+          .get({
+            url: "/community/houseBet",
+            headers: { "Content-Type": "application/json;charset=UTF-8" },
+            token: false,
+            qs: true,
+            data: {
+              communityName: query
+            }
+          })
+          .then(e => {
+            console.log(e.data);
+            if (e.data.code == 200) {
+              that.loading = false;
+              that.options = e.data.data.list;
+            }
+          });
       } else {
         that.options = [];
       }
     },
     queryCBId () {
-      var that = this
-      that.data.cbId = ""
-      that.data.roomNo = ''
-      this.$api.get({
-        url: "/mateHouse/queryComBuilding",
-        headers: { "Content-Type": "application/json;charset=UTF-8" },
-        token: false,
-        qs: true,
-        data: {
-          comId: that.data.comId
-        }
-      }).then((e) => {
-        if (e.data.code == 200) {
-          that.cbIdList = e.data.data.list;
-        }
-      })
+      var that = this;
+      that.data.cbId = "";
+      that.data.roomNo = "";
+      this.$api
+        .get({
+          url: "/mateHouse/queryComBuilding",
+          headers: { "Content-Type": "application/json;charset=UTF-8" },
+          token: false,
+          qs: true,
+          data: {
+            comId: that.data.comId
+          }
+        })
+        .then(e => {
+          if (e.data.code == 200) {
+            that.cbIdList = e.data.data.list;
+          }
+        });
     },
     queryRoomNo () {
-      var that = this
-      that.data.roomNo = ''
-      this.$api.get({
-        url: "/mateHouse/queryBuildIngHouses",
-        headers: { "Content-Type": "application/json;charset=UTF-8" },
-        token: false,
-        qs: true,
-        data: {
-          comId: that.data.comId,
-          cbId: that.data.cbId
-        }
-      }).then((e) => {
-        if (e.data.code == 200) {
-          that.roomNoList = e.data.data.list;
-        }
-      })
+      var that = this;
+      that.data.roomNo = "";
+      this.$api
+        .get({
+          url: "/mateHouse/queryBuildIngHouses",
+          headers: { "Content-Type": "application/json;charset=UTF-8" },
+          token: false,
+          qs: true,
+          data: {
+            comId: that.data.comId,
+            cbId: that.data.cbId
+          }
+        })
+        .then(e => {
+          if (e.data.code == 200) {
+            that.roomNoList = e.data.data.list;
+          }
+        });
     },
     queryHouseBet (currentPage, column, type) {
-      if(!column){
-        column="id"
+      if (!column) {
+        column = "id";
       }
-      if(!type){
-        type="ascending"
+      if (!type) {
+        type = "ascending";
       }
       var that = this;
       that.loading = true;
-      let params = { "limit": that.pageJson.pageSize, "page": currentPage };
+      let params = { limit: that.pageJson.pageSize, page: currentPage };
       params.sortColumn = column;
       params.sortType = type;
       if (Object.keys(this.moreSelect).length != 0) {
         for (let key in this.moreSelect) {
-          if (this.key == 'addTime' && this.moreSelect[key] !== '') {
+          if (this.key == "addTime" && this.moreSelect[key] !== "") {
             params.biginTime = this.moreSelect[key][0];
             params.endTime = this.moreSelect[key][1];
-          } else if (this.key == 'followTime' && this.moreSelect[key] !== '') {
+          } else if (this.key == "followTime" && this.moreSelect[key] !== "") {
             params.biginFollowTime = this.moreSelect[key][0];
             params.endFollowTime = this.moreSelect[key][1];
           } else {
-            params[key] = this.moreSelect[key]
+            params[key] = this.moreSelect[key];
           }
         }
-      }
-      else {
+      } else {
         params.comId = that.data.comId;
         params.cbId = that.data.cbId;
         params.roomNo = that.data.roomNo;
@@ -542,40 +532,38 @@ export default {
           params.endTime = that.data.timeSelect[1];
       }
       console.log(params);
-      this.$api.post({
-        // url: '/house/bet/list', // sql
-        url: '/house/bet/getBetHouse',// es
-        headers: { "Content-Type": "application/json;charset=UTF-8" },
-        data: params,
-        token: false
-      }).then((e) => {
-        console.log(e.data);
-        that.loading = false;
-        let data = e.data
-        if (data.code == 200) {
-          that.pageJson.total = data.data.totalCount;
-          that.pageJson.currentPage = data.data.currPage;
-          that.tableData = data.data.list;
-        } else {
-          console.log("查询对赌房源列表结果：" + result.message);
-          alert(result.message);
-        }
-      }).catch((e) => {
-        console.log("查询对赌房源列表失败");
-        console.log(e);
-      })
+      this.$api
+        .post({
+          url: "/house/bet/getBetHouse",
+          headers: { "Content-Type": "application/json;charset=UTF-8" },
+          data: params,
+          token: false
+        })
+        .then(e => {
+          console.log(e.data);
+          that.loading = false;
+          let data = e.data;
+          if (data.code == 200) {
+            that.pageJson.total = data.data.totalCount;
+            that.pageJson.currentPage = data.data.currPage;
+            that.tableData = data.data.list;
+          } else {
+            console.log("查询对赌房源列表结果：" + result.message);
+            alert(result.message);
+          }
+        })
+        .catch(e => {
+          console.log("查询对赌房源列表失败");
+          console.log(e);
+        });
     },
     isForBut (type) {
-      let array = [
-        { name: '查看', isType: '3', methosName: '' }
-      ]
-      return array.filter((item) => {
-        return item.isType.includes(type)
-      })
+      let array = [{ name: "查看", isType: "3", methosName: "" }];
+      return array.filter(item => {
+        return item.isType.includes(type);
+      });
     },
-    handleClick () {
-
-    },
+    handleClick () { },
     queryTabData () {
       this.$emit("queryTabData");
       console.log(this.queryData);
@@ -583,13 +571,13 @@ export default {
     },
     handleCurrentChange (val) {
       console.log(`当前页: ${val}`);
-      this.queryHouseBet(val, 'id', 'descending');
+      this.queryHouseBet(val, "id", "descending");
     },
     handleSizeChange (val) {
       console.log(`每1页 ${val} 条`);
       this.pageJson.pageSize = val;
-      this.queryHouseBet(1, 'id', 'ascending');
+      this.queryHouseBet(1, "id", "ascending");
     }
-  },
-}
+  }
+};
 </script>
