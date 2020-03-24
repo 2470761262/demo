be44f7f769560a99e9a4eff82732ffd9ca2255f5
xnq <1244960280@qq.com>
2020-03-24 15:25:09 +0800

    新增参数

--- src/minxi/getMenuRid.js
+++ src/minxi/getMenuRid.js
@@ -1,33 +1,33 @@
 import {
-  LOGINDATA
+    LOGINDATA
 } from '@/util/constMap';
 import util from '@/util/util';
 export default {
-  created() {
+    created () {
 
-    this.resultRid(util.localStorageGet(LOGINDATA).menuNodes, this.$route.path);
-  },
-  methods: {
-    deepGetRid(list, nowPathName) {
-      let flag = false;
-      for (let i = 0; i < list.length; i++) {
-        if (list[i].rurl == nowPathName) {
-          this.$store.commit("setNowRid", list[i].id);
-          flag = true;
-        } else {
-          if (list[i].children) {
-            if (this.deepGetRid(list[i].children, nowPathName)) {
-              return true;
-            }
-          }
-        }
-      }
-      return flag;
+        // this.resultRid(util.localStorageGet(LOGINDATA).menuNodes, this.$route.path);
     },
-    resultRid(list, nowPathName) {
-      if (!this.deepGetRid(list, nowPathName)) {
-        this.$store.commit("setNowRid", 15); //null);
-      }
+    methods: {
+        deepGetRid (list, nowPathName) {
+            let flag = false;
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].rurl == nowPathName) {
+                    this.$store.commit("setNowRid", list[i].id);
+                    flag = true;
+                } else {
+                    if (list[i].children) {
+                        if (this.deepGetRid(list[i].children, nowPathName)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return flag;
+        },
+        resultRid (list, nowPathName) {
+            if (!this.deepGetRid(list, nowPathName)) {
+                this.$store.commit("setNowRid", 15); //null);
+            }
+        }
     }
-  }
 }
