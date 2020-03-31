55b624423ac8c6a1d28660844554d78a580e0b71
247076126@qq.com <247076126@qq.com>
2020-03-31 17:20:58 +0800

    update

--- src/util/util.js
+++ src/util/util.js
@@ -102,5 +102,50 @@ export default {
         _s = s < 10 ? '0' + s : s + '';
         _m = m < 10 ? '0' + m : m + '';
         return _m + ":" + _s;
+    },
+    //节流
+    throttle (delay, callback) {
+        let timeoutID;
+        let lastExec = 0;
+
+        function wrapper () {
+            const self = this;
+            const elapsed = Number(new Date()) - lastExec;
+            const args = arguments;
+
+            function exec () {
+                lastExec = Number(new Date());
+                callback.apply(self, args);
+            }
+
+            clearTimeout(timeoutID);
+
+            if (elapsed > delay) {
+                exec();
+            } else {
+                timeoutID = setTimeout(exec, delay - elapsed);
+            }
+        }
+
+        return wrapper;
+    },
+    //防抖
+    debounce (delay, callback) {
+        let timeoutID;
+
+        function wrapper () {
+            const self = this;
+            const args = arguments;
+
+            function exec () {
+                callback.apply(self, args);
+            }
+
+            clearTimeout(timeoutID);
+
+            timeoutID = setTimeout(exec, delay);
+        }
+
+        return wrapper;
     }
 }
