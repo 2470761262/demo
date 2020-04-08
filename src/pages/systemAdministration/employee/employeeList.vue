f22dc9f4b575020816b83444de1aa9422bd5ce00
wlh <731885770@qq.com>
2020-04-08 22:01:25 +0800

    待审核

--- src/pages/systemAdministration/employee/employeeList.vue
+++ src/pages/systemAdministration/employee/employeeList.vue
@@ -436,7 +436,7 @@ export default {
       params.leaveTime = this.leaveTime; 
       const loading = this.$loading({
           lock: true,
-          text: 'Loading',
+          text: '请等待..',
           spinner: 'el-icon-loading',
           background: 'rgba(0, 0, 0, 0.7)'
         });
