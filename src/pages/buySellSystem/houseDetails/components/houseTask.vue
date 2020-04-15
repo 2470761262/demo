762dc16a00d4e960599f8d8b58f0da6551714e38
247076126@qq.com <247076126@qq.com>
2020-04-15 14:27:14 +0800

    update

--- src/pages/buySellSystem/houseDetails/components/houseTask.vue
+++ src/pages/buySellSystem/houseDetails/components/houseTask.vue
@@ -243,6 +243,7 @@
     <!-- 上传 -->
     <fixedPopup :visible.sync="houseUploadflag"
                 title=""
+                v-if="houseUploadflag"
                 width="960px">
       <houseUploadExtends ref="houseUpload"
                           :replaceType="houseUploadType"></houseUploadExtends>
@@ -290,10 +291,10 @@ import util from "@/util/util";
 export default {
   inject: ["houseDetails", "houseId", "buttonDisabled"],
   computed: {
-    isDisabled () {
+    isDisabled() {
       return this.buttonDisabled;
     },
-    resultData () {
+    resultData() {
       if (Object.keys(this.houseDetails).length > 0) {
         return this.houseDetails.data;
       } else {
@@ -307,7 +308,7 @@ export default {
     entrustPop,
     supplement
   },
-  data () {
+  data() {
     return {
       houseUploadLoading: false,
       houseUploadflag: false,
@@ -326,7 +327,7 @@ export default {
     };
   },
   filters: {
-    mapFilter (value, ListName, resultValue = null) {
+    mapFilter(value, ListName, resultValue = null) {
       return util.countMapFilter(value, ListName, resultValue);
     }
   },
@@ -334,7 +335,7 @@ export default {
     /**
      * 申请跟单人
      */
-    applyAgent () {
+    applyAgent() {
       let params = this.$refs.com.formData;
       let that = this;
       this.$refs.com.validateAllNotUpdata().then(e => {
@@ -368,14 +369,14 @@ export default {
                 that.$message(result.message);
               }
             })
-            .catch(e => { });
+            .catch(e => {});
         }
       });
     },
     /**
      * 申请跟单人打开弹窗
      */
-    openAgentPop () {
+    openAgentPop() {
       if (this.resultData.applyAgentVo != null) {
         this.$store.commit("updateStep2", this.resultData.applyAgentVo);
         this.audioList = this.resultData.applyAgentVo.saleUploadAudioList;
@@ -397,7 +398,7 @@ export default {
      * @param {String} popName 弹出层的Flag名字
      * @param {number} type 打开类型
      */
-    async openPop (popName, type, typeName, replaceType) {
+    async openPop(popName, type, typeName, replaceType) {
       if (type != 4) {
         let result = await houseCheck.isChecking(
           type,
@@ -434,7 +435,7 @@ export default {
     /**
      * refs 获取上传组件实例并且验证非空
      */
-    submitUpload () {
+    submitUpload() {
       let _that = this;
       let verifyFieldMap = new Map([
         ["outdoorImgList", "外景图"],
@@ -451,7 +452,7 @@ export default {
             name: _key,
             alias: _value,
             rules: "required",
-            getter: function () {
+            getter: function() {
               if (_that.$refs.houseUpload[_key] instanceof Array) {
                 return _that.$refs.houseUpload[_key];
               } else {
@@ -471,7 +472,7 @@ export default {
         } else {
           let url = `/agentHouse/propertyCheck/${
             this.houseUploadType == 12 ? "insertApplyFor" : "insertReplace"
-            }`;
+          }`;
           let resultIdList = [];
           verifyFieldMap.forEach((_value, _key) => {
             if (_that.$refs.houseUpload[_key] instanceof Array) {
@@ -500,7 +501,7 @@ export default {
                 this.$message.success(e.data.message);
               }
             })
-            .catch(e => { })
+            .catch(e => {})
             .finally(() => {
               this.houseUploadLoading = false;
               this.houseUploadflag = false;
