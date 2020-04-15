c6bfe59c23323f21c642939f7265a5aa855a400c
XL <1598675282@qq.com>
2020-04-15 17:21:11 +0800

    Signed-off-by: XL <1598675282@qq.com>

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  VUE_APP_BASE_API: '"http://bweb.yongxinjia.com/"' //项目api地址
+    NODE_ENV: '"production"',
+    VUE_APP_BASE_API: '"http://bweb.yongxinjia.com/"' //项目api地址
 }
