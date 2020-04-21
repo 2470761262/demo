4508576919f375af3a26e7601ed77ef73e51249e
247076126@qq.com <247076126@qq.com>
2020-04-21 10:39:05 +0800

    update

--- src/pages/customersSystem/myCustomers/allCustomers.vue
+++ src/pages/customersSystem/myCustomers/allCustomers.vue
@@ -1,17 +1,39 @@
 <style lang="less" scoped>
+.page-tab-title {
+  color: #666666;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  .iconfont {
+    font-size: 23px;
+    margin-right: 10px;
+  }
+}
 </style>
 <template>
   <list-Page @handleCurrentChange="handleCurrentChange"
              :parentData="$data"
              @sort-change="sortMethod"
-             @handleSizeChange="handleSizeChange"></list-Page>
+             @handleSizeChange="handleSizeChange">
+    <template v-slot:top>
+      <allCustomersQuery></allCustomersQuery>
+    </template>
+    <template v-slot:title>
+      <h3 class="page-tab-title"><i class="iconzaishouwugendan iconfont"></i> <span>客源列表</span></h3>
+    </template>
+    <template v-slot:tableColumn>
+
+    </template>
+  </list-Page>
 </template>
 
 <script>
 import listPage from "@/components/listPage";
+import allCustomersQuery from "./components/allCustomersQuery";
 export default {
   components: {
-    listPage
+    listPage,
+    allCustomersQuery
   },
   data() {
     return {
