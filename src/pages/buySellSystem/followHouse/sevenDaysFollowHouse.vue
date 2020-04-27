463c7d2fd855e823aace211d5518194407d44a4a
lhp <lhp993356686@qq.com>
2020-04-27 21:24:16 +0800

    7天被带看

--- src/pages/buySellSystem/followHouse/sevenDaysFollowHouse.vue
+++ src/pages/buySellSystem/followHouse/sevenDaysFollowHouse.vue
@@ -7,90 +7,101 @@
 </style>
 <template>
   <div class="page-content">
-    <list-page :parentData="$data"
-               @handleSizeChange="handleSizeChange"
-               @handleCurrentChange="querylist">
+    <list-page
+      :parentData="$data"
+      @handleSizeChange="handleSizeChange"
+      @handleCurrentChange="querylist"
+    >
       <template v-slot:top>
         <div class="page-list-query-row">
           <div class="query-content-cell">
             <h3 class="query-cell-title">楼盘</h3>
-            <el-select v-model="queryData.CommunityName"
-                       @focus="remoteCommunityFoucus"
-                       remote
-                       clearable
-                       placeholder="楼盘名称"
-                       @change="querylist(null)"
-                       :remote-method="remoteCommunity"
-                       :loading="communitySelect.loading">
-              <el-option v-for="item in communitySelect.list"
-                         :key="item.value"
-                         :label="item.name"
-                         :value="item.name"></el-option>
+            <el-select
+              v-model="queryData.CommunityName"
+              @focus="remoteCommunityFoucus"
+              remote
+              clearable
+              placeholder="楼盘名称"
+              @change="querylist(null)"
+              :remote-method="remoteCommunity"
+              :loading="communitySelect.loading"
+            >
+              <el-option
+                v-for="item in communitySelect.list"
+                :key="item.value"
+                :label="item.name"
+                :value="item.name"
+              ></el-option>
             </el-select>
           </div>
           <div class="query-content-cell cell-interval75">
             <h3 class="query-cell-title">业主</h3>
-            <el-input placeholder="姓名"
-                      @change="querylist(null)"
-                      class="set-input120"
-                      v-model="queryData.Customers"
-                      clearable />
+            <el-input
+              placeholder="姓名"
+              @change="querylist(null)"
+              class="set-input120"
+              v-model="queryData.Customers"
+              clearable
+            />
           </div>
           <div class="query-content-cell cell-interval45">
             <h3 class="query-cell-title">电话</h3>
-            <el-input placeholder="业主电话"
-                      v-number
-                      @change="querylist(null)"
-                      v-model="queryData.tel"
-                      class="set-input200"
-                      clearable />
+            <el-input
+              placeholder="业主电话"
+              v-number
+              @change="querylist(null)"
+              v-model="queryData.tel"
+              class="set-input200"
+              clearable
+            />
           </div>
           <div class="query-content-cell cell-interval45">
             <h3 class="query-cell-title">录入时间</h3>
-            <el-date-picker v-model="queryData.timeSelect"
-                            type="daterange"
-                            class="set-data-pricker"
-                            range-separator="至"
-                            @change="querylist(null)"
-                            start-placeholder="开始日期"
-                            :default-time="['00:00:00', '23:59:59']"
-                            end-placeholder="结束日期"></el-date-picker>
+            <el-date-picker
+              v-model="queryData.timeSelect"
+              type="daterange"
+              class="set-data-pricker"
+              range-separator="至"
+              @change="querylist(null)"
+              start-placeholder="开始日期"
+              :default-time="['00:00:00', '23:59:59']"
+              end-placeholder="结束日期"
+            ></el-date-picker>
           </div>
           <div class="query-content-cell cell-interval45">
             <h3 class="query-cell-title">房源类型</h3>
-            <el-select clearable
-                       placeholder="房源类型"
-                       class="set-select110"
-                       v-model="queryData.houseStates">
-              <el-option v-for="item in houseStatesList"
-                         :key="item.value"
-                         :label="item.label"
-                         :value="item.value"></el-option>
+            <el-select
+              clearable
+              placeholder="房源类型"
+              class="set-select110"
+              v-model="queryData.houseStates"
+            >
+              <el-option
+                v-for="item in houseStatesList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
             </el-select>
           </div>
           <div class="query-content-cell cell-interval45">
-            <el-button type="primary"
-                       size="mini"
-                       @click="querylist(null)">查询</el-button>
+            <el-button type="primary" size="mini" @click="querylist(null)">查询</el-button>
           </div>
         </div>
       </template>
       <template v-slot:tableColumn>
-        <el-table-column v-for="item in tableDataColumn"
-                         :key="item.prop"
-                         :label="item.label"
-                         :width="item.width"
-                         :formatter="item.formart"
-                         show-overflow-tooltip
-                         :prop="item.prop">
-        </el-table-column>
-        <el-table-column label="操作"
-                         fixed="right"
-                         width="90">
+        <el-table-column
+          v-for="item in tableDataColumn"
+          :key="item.prop"
+          :label="item.label"
+          :width="item.width"
+          :formatter="item.formart"
+          show-overflow-tooltip
+          :prop="item.prop"
+        ></el-table-column>
+        <el-table-column label="操作" fixed="right" width="90">
           <template v-slot="scope">
-            <el-button type="primary"
-                       size="mini"
-                       @click="navPage(scope.row.id)">查看</el-button>
+            <el-button type="primary" size="mini" @click="navPage(scope.row.id)">查看</el-button>
           </template>
         </el-table-column>
       </template>
@@ -105,7 +116,7 @@ export default {
   components: {
     listPage
   },
-  data () {
+  data() {
     return {
       loading: false, //控制表格加载动画提示
       pageJson: {
@@ -114,25 +125,26 @@ export default {
         pageSize: 10 //每页条数
       },
       tableDataColumn: [
-        { prop: "HouseNo", label: "房源编号", width: "" },
-        { prop: "CommunityName", label: "楼盘名称", width: "" },
-        { prop: "Price", label: "售价(万元)", width: "160" },
-        { prop: "InArea", label: "面积(m²)", width: "160" },
+        { prop: "houseNo", label: "房源编号", width: "" },
+        { prop: "communityName", label: "楼盘名称", width: "" },
+        { prop: "price", label: "售价(万元)", width: "160" },
+        { prop: "inArea", label: "面积(m²)", width: "160" },
         {
           label: "单价(元/㎡)",
           width: "160",
           formart: item =>
-            Math.round((item.Price * 10000) / item.InArea) + "元/m²"
+            Math.round((item.price * 10000) / item.inArea) + "元/m²"
         },
         {
           prop: "hall",
           label: "户型",
           width: "170",
           formart: item =>
-            item.Rooms + "室" + item.hall + "厅" + item.toilet + "卫"
+            item.rooms + "室" + item.hall + "厅" + item.toilet + "卫"
         },
-        { prop: "Decoration", label: "装修程度", width: "160" },
-        { prop: "FollowEndTime", label: "最后带看时间", width: "160" }
+        { prop: "decoration", label: "装修程度", width: "160" }
+        // ,
+        // { prop: "FollowEndTime", label: "最后带看时间", width: "160" }
       ],
       tableData: [],
       communitySelect: {
@@ -156,23 +168,23 @@ export default {
       }
     };
   },
-  created () {
+  created() {
     this.querylist();
   },
   methods: {
-    navPage (id) {
+    navPage(id) {
       this.$router.push({ name: "houseDetails", params: { houseId: id } });
     },
-    handleSizeChange (e) {
+    handleSizeChange(e) {
       this.pageJson.pageSize = e;
       this.querylist();
     },
-    remoteCommunityFoucus () {
+    remoteCommunityFoucus() {
       if (this.communitySelect.list.length == 0) {
         this.remoteCommunity();
       }
     },
-    remoteCommunity (query = "") {
+    remoteCommunity(query = "") {
       this.communitySelect.loading = true;
       this.$api
         .post({
@@ -193,7 +205,7 @@ export default {
           this.communitySelect.loading = false;
         });
     },
-    querylist (currentPage) {
+    querylist(currentPage) {
       let _that = this;
       let params = {
         limit: _that.pageJson.pageSize,
@@ -208,16 +220,18 @@ export default {
       this.loading = true;
       this.$api
         .get({
-          url: "/agent_house/listFollowHouse",
+          //   url: "/agent_house/doubleclick/listFollowHouse",
+          url: "/mateHouse/getMateHouse/sevenDayHouse",
           headers: { "Content-Type": "application/json;charset=UTF-8" },
           data: params
         })
         .then(e => {
           let result = e.data;
           if (result.code == 200) {
+            console.log("result.code", result.code);
             this.pageJson.total = result.data.totalCount;
             this.pageJson.currentPage = result.data.currPage;
-            this.tableData = result.data.list;
+            this.tableData = result.data.data;
           } else {
             return Promise.reject(result.message);
           }
