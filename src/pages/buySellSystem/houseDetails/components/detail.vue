ac3fe918ab23abe3caf7f5a404a1793a4f6a76ad
辛苦了 <247076126@qq.com>
2020-05-11 19:28:09 +0800

    修改历史房源转在售为可跳转

--- src/pages/buySellSystem/houseDetails/components/detail.vue
+++ src/pages/buySellSystem/houseDetails/components/detail.vue
@@ -306,7 +306,7 @@
         <el-button
           class="cell-pro-but"
           v-if="applyAgentRule"
-          :disabled="isDisabled || agentApply"
+          :disabled="isDisabled || agentApply || housePageType == 'notSale'"
           @click="callTaskAgent"
           >{{ housePageType != "notSale" ? "申请跟单人" : "转在售" }}</el-button
         >
