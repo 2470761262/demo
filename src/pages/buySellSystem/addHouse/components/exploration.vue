d0357b737dc5e07f80fba268cbd3b049eba12b7e
hxf <hxf@qq.com>
2020-05-11 16:13:56 +0800

    好伤心

--- src/pages/buySellSystem/addHouse/components/exploration.vue
+++ src/pages/buySellSystem/addHouse/components/exploration.vue
@@ -510,6 +510,7 @@ export default {
       let that = this;
       console.log(r, "接收到了消息");
       if (r.content.resourceType == "vedio") {
+        debugger
         console.log(r.content, "视频消息内容，准备插入草稿箱");
         that.uploadFileInfo(undefined, r.content.picUrl, function(data) {
           if (that.houseVideo.id) {
