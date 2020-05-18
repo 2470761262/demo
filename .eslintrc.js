0e1f9255622789835103ee7744fce28edb484967
hxf <hxf@qq.com>
2020-05-18 14:18:52 +0800

    连接状态

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "off" : "off",
     "no-unused-vars": 'off'
   },
   parserOptions: {
