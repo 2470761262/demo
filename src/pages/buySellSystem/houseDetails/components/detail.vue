85c7f0bb4ec1a7fdead77832bc316208c4e351d8
齐天大圣 <zgw@qq.com>
2020-05-28 23:59:25 +0800

    显示错误

--- src/pages/buySellSystem/houseDetails/components/detail.vue
+++ src/pages/buySellSystem/houseDetails/components/detail.vue
@@ -184,7 +184,7 @@
   <div class="detail-content">
     <div class="detail-price-content">
       <div class="detail-price">
-        <span>{{ Math.trunc(resultData.tradePrice) || Math.trunc(resultData.Price) }}</span>
+        <span>{{ resultData.tradePrice || resultData.Price }}</span>
         <span v-if="resultData.tradePrice || resultData.Price">万元</span>
       </div>
       <div class="detail-price-avg">
