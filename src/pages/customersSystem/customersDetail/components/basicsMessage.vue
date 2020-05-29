c64fd09abf04844c303fd48199e6c242694656b6
ywz <1094769835@qq.com>
2020-05-29 11:20:55 +0800

    客户详情基础信息

--- src/pages/customersSystem/customersDetail/components/basicsMessage.vue
+++ src/pages/customersSystem/customersDetail/components/basicsMessage.vue
@@ -41,64 +41,196 @@
     <div class="basics-layout-content">
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">客户籍贯</div>
-        <div class="basics-layout-item-message">新罗区</div>
+        <div class="basics-layout-item-message" v-html="detail.areaId || kong">
+          新罗区
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">客户来源</div>
-        <div class="basics-layout-item-message">鑫家网</div>
+        <div class="basics-layout-item-message" v-html="detail.source || kong">
+          鑫家网
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">期望面积</div>
-        <div class="basics-layout-item-message">80-80平</div>
+        <div class="basics-layout-item-message" v-html="detail.hopeArea">
+          1-1平
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">首付金额</div>
-        <div class="basics-layout-item-message">50-60万</div>
+        <div class="basics-layout-item-message" v-html="detail.hopeFirtPay">
+          1-1
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">期望总价</div>
-        <div class="basics-layout-item-message">100-120万</div>
+        <div class="basics-layout-item-message" v-html="detail.hopeAllPay">
+          1-1万
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">付款方式</div>
-        <div class="basics-layout-item-message">商业贷款</div>
+        <div class="basics-layout-item-message">
+          未填写
+        </div>
       </div>
       <div class="basics-layout-item">
-        <div class="basics-layout-item-title">购买户型</div>
-        <div class="basics-layout-item-message">3房</div>
+        <div class="basics-layout-item-title">
+          购买户型
+        </div>
+        <div class="basics-layout-item-message" v-html="detail.rooms || kong">
+          3房
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">装修需求</div>
-        <div class="basics-layout-item-message">毛坯</div>
+        <div
+          class="basics-layout-item-message"
+          v-html="detail.decoration || kong"
+        >
+          毛坯
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">期望小学</div>
-        <div class="basics-layout-item-message">未填写</div>
+        <div class="basics-layout-item-message" v-html="detail.school1 || kong">
+          未填写
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">期望中学</div>
-        <div class="basics-layout-item-message">未填写</div>
+        <div class="basics-layout-item-message" v-html="detail.school2 || kong">
+          未填写
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">上次带看</div>
-        <div class="basics-layout-item-message">2020-03-01</div>
+        <div
+          class="basics-layout-item-message"
+          v-html="detail.lastPairFollowTime || kong"
+        >
+          2020-03-01
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">录入时间</div>
-        <div class="basics-layout-item-message">2020-01-01</div>
+        <div class="basics-layout-item-message" v-html="detail.addTime || kong">
+          2020-01-01
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">录入人</div>
-        <div class="basics-layout-item-message">钟丽娟</div>
+        <div class="basics-layout-item-message" v-html="detail.addName || kong">
+          钟丽娟
+        </div>
       </div>
       <div class="basics-layout-item">
         <div class="basics-layout-item-title">PASS方</div>
-        <div class="basics-layout-item-message">暂无</div>
+        <div
+          class="basics-layout-item-message"
+          v-html="detail.passOwnerName || kong"
+        >
+          暂无
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+import { mapState, mapMutations } from "vuex";
+import moment from "moment";
+export default {
+  data() {
+    return {
+      kong: "未填写"
+    };
+  },
+  // watch:{
+  //   detail: {
+  //     immediate: true,
+  //     deep: true,
+  //     handler(newValue) {
+
+  //     }
+  // },
+  computed: {
+    ...mapState({
+      detail: value => {
+        const kong = "未填写";
+        const kongValue = 1;
+        let dc = value.customers.detail.cusDetail;
+        //因为detail会在数据没有加进去时就运行一次，所以第一次必然为空，会报错,因此要做一个非空判断，非空再来做操作
+        if (dc) {
+          dc = dc.data;
+          //期望面积
+          if (dc.minArea == null) {
+            dc.minArea = kongValue;
+          }
+          if (dc.maxArea == null) {
+            dc.maxArea = kong;
+          }
+          dc.hopeArea = dc.minArea + "-" + dc.maxArea + "平";
+
+          //首付金额
+          if (dc.minFirstPrice == null) {
+            dc.minFirstPrice = kongValue;
+          }
+          if (dc.maxFirstPrice == null) {
+            dc.maxFirstPrice = kongValue;
+          }
+          dc.hopeFirtPay = dc.minFirstPrice + "-" + dc.maxFirstPrice + "万";
+
+          //期望总价
+          if (dc.minPrice == null) {
+            dc.minPrice = kong;
+          }
+          if (dc.maxPrice == null) {
+            dc.maxPrice = kong;
+          }
+          dc.hopeAllPay = dc.minPrice + "-" + dc.maxPrice + "万";
+          //装修需求
+          let reg = new RegExp("\\$", "g");
+          if (dc.decoration.indexOf("$") != -1) {
+            dc.decoration = dc.decoration.replace(reg, ",");
+            dc.decoration = dc.decoration.substr(0, dc.decoration.length - 1);
+          }
+
+          //购买户型
+          if (dc.rooms.indexOf("$") != -1) {
+            dc.rooms = dc.rooms.replace(reg, ",");
+            dc.rooms = dc.rooms.substr(0, dc.rooms.length - 1);
+          }
+          //期望中学
+          if (dc.school1.indexOf("$") != -1) {
+            dc.school1 = dc.school1.replace(reg, ",");
+            dc.school1 = dc.school1.substr(0, dc.school1.length - 1);
+          }
+
+          //期望小学
+          if (dc.school2.indexOf("$") != -1) {
+            dc.school2 = dc.school2.replace(reg, ",");
+            dc.school2 = dc.school2.substr(0, dc.school2.length - 1);
+          }
+
+          //上次带看
+          if (dc.lastPairFollowTime)
+            dc.lastPairFollowTime = moment(dc.lastPairFollowTime).format(
+              "YYYY-MM-DD"
+            );
+          //录入时间
+          if (dc.addTime) dc.addTime = moment(dc.addTime).format("YYYY-MM-DD");
+        }
+
+        console.log("---------------", dc);
+        return dc ? dc : {};
+      }
+    })
+  },
+  mounted() {},
+  methods: {
+    init() {}
+  }
+};
 </script>
--- src/pages/customersSystem/customersDetail/customersDetail.vue
+++ src/pages/customersSystem/customersDetail/customersDetail.vue
@@ -68,7 +68,13 @@ import takeLookRecord from "./components/takeLookRecord";
 //跟进记录 & 推荐记录
 import follow from "./components/follow";
 import { mapState, mapMutations } from "vuex";
+import util from "@/util/util";
 export default {
+  data() {
+    return {
+      queryParams: []
+    };
+  },
   components: {
     detailButton,
     basics,
@@ -84,18 +90,42 @@ export default {
     })
   },
   mounted() {
-    this.ajax(343);
+    this.ajax();
   },
   methods: {
     ...mapMutations(["updateDetail", "resetDetail"]),
     ajax() {
-      this.$store.commit("updateDetail", {
-        name: 1,
-        age: 5
-      });
-
-      //this.detail
-      console.log("ajax -> this.detail", this.detail);
+      let _that = this;
+      //获取页面传过来的客户id
+      let id = _that.$route.params.customerId;
+      //因为rout.push传值一刷新就会消失，所以需要保存到session里
+      if (id != null) {
+        util.sessionLocalStorageSet("cosDetail:id", id);
+      } else {
+        id = util.sessionLocalStorageGet("cosDetail:id");
+      }
+      //执行ajax请求，获取基础信息
+      _that.$api
+        .post({
+          url: "/saleCustomerDetail/getACusDetail",
+          data: { id: id },
+          headers: { "Content-Type": "application/json" }
+        })
+        .then(e => {
+          let result = e.data;
+          console.log("获取客户详情结果", e);
+          if (result.code == 200) {
+            //result.data.pageSum
+            this.$store.commit("updateDetail", {
+              cusDetail: result
+            });
+          }
+        })
+        .catch(e => {
+          console.log("获取客户详情失败");
+          console.log(e);
+        })
+        .finally(() => {});
     }
   },
   destroyed() {
