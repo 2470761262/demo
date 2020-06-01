cb20ef9981f9eaebbda454a286a9a30c8ca833fd
wlh <731885770@qq.com>
2020-06-01 14:49:10 +0800

    添加印象以及转状态窗口

--- src/pages/customersSystem/customersDetail/didLog/turnType.vue
+++ src/pages/customersSystem/customersDetail/didLog/turnType.vue
@@ -13,11 +13,10 @@
 <template>
   <fixed-popup v-bind="$attrs" v-on="$listeners" @confirmEmit="confirmEmit">
     <template>
-      <strong class="error-tips">{{ errorBags.all()[0] }}</strong>
       <div class="belt-content">
         <div class="belt-content-item">
           <div class="item-right">
-            <el-radio-group v-model="status" @change="statusChange">
+            <el-radio-group v-model="formData.isBuy" @change="statusChange">
               <el-radio-button label="已购"></el-radio-button>
               <el-radio-button label="暂不购"></el-radio-button>
             </el-radio-group>
@@ -27,9 +26,15 @@
           <div class="item-right">
             <div class="check-content">
               <div class="label-content">
-                <el-radio v-model="isTalk" :label="0">我司成交</el-radio>
-                <el-radio v-model="isTalk" :label="1">他司成交</el-radio>
-                <el-radio v-model="isTalk" :label="1">客户自购</el-radio>
+                <el-radio v-model="formData.buyWay" :label="'我司成交'"
+                  >我司成交</el-radio
+                >
+                <el-radio v-model="formData.buyWay" :label="'他司成交'"
+                  >他司成交</el-radio
+                >
+                <el-radio v-model="formData.buyWay" :label="'客户自购'"
+                  >客户自购</el-radio
+                >
               </div>
             </div>
           </div>
@@ -38,12 +43,28 @@
           <div class="item-right">
             <div class="check-content">
               <span>成交楼盘</span>
-              <el-input style="width:60%;margin-left:20px"></el-input>
+              <el-select
+                v-model="formData.community"
+                clearable
+                filterable
+                remote
+                :remote-method="queryCommunityByKeyWord"
+                :loading="searchLoading"
+                placeholder="请选择成交楼盘"
+              >
+                <el-option
+                  v-for="item in communityList"
+                  :key="item.value"
+                  :label="item.name"
+                  :value="item.value + ':' + item.name"
+                ></el-option>
+              </el-select>
             </div>
             <div class="check-content">
               <span>成交金额</span>
               <el-input
                 clearable
+                v-model="formData.buyAmount"
                 style="width:60%;margin-left:20px"
                 placeholder="成交金额"
               ></el-input>
@@ -51,8 +72,8 @@
             <div class="check-content">
               <span>成交时间</span>
               <el-date-picker
-                v-model="value1"
                 type="date"
+                v-model="formData.buyTime"
                 placeholder="选择日期"
               >
               </el-date-picker>
@@ -66,9 +87,6 @@
 
 <script>
 export default {
-  $_veeValidate: {
-    validator: "new" // give me my own validator scope.
-  },
   components: {},
   props: {
     customerId: {
@@ -79,18 +97,57 @@ export default {
   },
   data() {
     return {
-      textarea: "",
-      isTalk: 0, //是否再谈
-      accompanyResult: [], //陪同人
-      cascaderResult: [], //带看楼盘
-      beltTime: [], //带看时间
+      formData: {
+        buyAmount: null,
+        buyTime: null,
+        buyWay: "我司成交",
+        isBuy: "已购", //1已购，2暂不购
+        community: ""
+      },
       status: null,
-      isVisible: false
+      isVisible: false,
+      searchLoading: false,
+      communityList: []
     };
   },
   methods: {
+    queryCommunityByKeyWord(query) {
+      if (query !== "") {
+        let _that = this;
+        this.searchLoading = true;
+        _that.$api
+          .get({
+            url: "/community/houseList",
+            qs: true,
+            data: { communityName: query }
+          })
+          .then(e => {
+            _that.searchLoading = false;
+            let result = e.data;
+            if (result.code == 200) {
+              console.log(result, "查询楼盘");
+              _that.communityList = result.data.list;
+            } else {
+              console.log("查询楼盘" + result.message);
+              _that.$message({
+                type: "info",
+                message: result.message
+              });
+            }
+          })
+          .catch(e => {
+            _that.searchLoading = false;
+            console.log("查询楼盘失败catch");
+            console.log(e);
+          })
+          .finally(() => {});
+      } else {
+        this.communityList = [];
+      }
+    },
     statusChange(e) {
-      console.log("切换了", e);
+      console.log("切换了", e, this.formData.isBuy);
+
       if (e == "已购") {
         this.isVisible = false;
       } else {
@@ -133,14 +190,27 @@ export default {
       // this.$validator.validateAll().then(e => {
       //   if (!e) return;
       // });
+      if (_that.formData.isBuy == "已购") {
+        _that.formData.isBuy = 1;
+      } else {
+        _that.formData.isBuy = 2;
+      }
+      if (_that.formData.isBuy == 1 && !_that.formData.community) {
+        _that.$message({
+          type: "info",
+          message: "请指定成交楼盘"
+        });
+        return;
+      }
+      console.log(this.formData);
       if (this.customerId && this.customerId != 0) {
         //在该组件内响应添加带看
         console.log(this.customerId, "添加带看在组件内响应");
       } else {
         console.log(
-          "添加带看在父组件内响应，请在父组件内注册事件confirmAddLook"
+          "添加带看在父组件内响应，请在父组件内注册事件confirmTurnType"
         );
-        _that.$emit("confirmAddLook", this.$data);
+        _that.$emit("confirmTurnType", this.$data);
       }
     }
   }
