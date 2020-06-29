bc7ee747fdb62b390000186a9d5b9dd5b1ab2ec0
247076126@qq.com <247076126@qq.com>
2020-06-29 08:57:11 +0800

    删除客源

--- src/router/routerResult/index.js
+++ src/router/routerResult/index.js
@@ -517,24 +517,6 @@ export default [
     ]
   },
   {
-    //客源系统 三级路由
-    path: "/customersSystem/customersIframe",
-    name: "customersIframe",
-    redirect: "/customersSystem/allCustomers",
-    meta: {
-      title: "客源系统"
-    },
-    component: () =>
-      import(
-        /* webpackChunkName: "customersIframe" */ "@/pages/customersSystem/customersIframe/customersIframe.vue"
-      ),
-    children: [
-     
-      
-    
-    ]
-  },
-  {
     //系统管理 公告管理
     path: "/sys/noticeManageList",
     meta: {
