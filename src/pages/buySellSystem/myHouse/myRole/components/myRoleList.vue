959d07c223e7c3def5d4817f38478542a685c1bf
ckx <296572823@qq.com>
2020-08-11 09:19:52 +0800

    Signed-off-by: ckx <296572823@qq.com>

--- src/pages/buySellSystem/myHouse/myRole/components/myRoleList.vue
+++ src/pages/buySellSystem/myHouse/myRole/components/myRoleList.vue
@@ -283,7 +283,7 @@ export default {
           label: "楼层",
           order: true,
           formart: item => {
-            return `${item.floor}/${item.floorNum}`;
+            return `${item.floor}/${item.floorNum || "暂无"}`;
           }
         },
         {
@@ -292,7 +292,7 @@ export default {
           order: true
         },
         {
-          prop: "brokerName",
+          prop: "agentName",
           label: "跟单人",
           order: false
         }
