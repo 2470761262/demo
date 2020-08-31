49d3b4ee1cbabdcbf83b8acb3630936e0fc84e13
lyn <728188325@qq.com>
2020-08-31 18:13:33 +0800

    锁定房源页面对接

--- src/pages/buySellSystem/managementEntrance/buySellHouse/lockingList.vue
+++ src/pages/buySellSystem/managementEntrance/buySellHouse/lockingList.vue
@@ -6,44 +6,96 @@
       <div class="conditions-box">
         <el-row :gutter="32">
           <el-form label-position="right" label-width="80px">
-            <el-col :span="8">
+            <el-col :span="12">
               <el-row :gutter="10">
                 <el-form-item label="楼盘">
                   <el-col :span="8">
-                    <el-select v-model="val1" placeholder="楼盘">
-                      <el-option label="区域一" value="shanghai"></el-option>
-                      <el-option label="区域二" value="beijing"></el-option>
+                    <el-select
+                      v-model="buildOptData"
+                      placeholder="楼盘"
+                      clearable
+                      filterable
+                      remote
+                      @focus="remoteBuildInput"
+                      @change="remoteBuildChange"
+                      :remote-method="buildRemoteMethod"
+                      :loading="buildLoading"
+                      value-key="value"
+                    >
+                      <el-option
+                        v-for="item in buildForList"
+                        :key="item.value"
+                        :label="item.name"
+                        :value="item"
+                      ></el-option>
                     </el-select>
                   </el-col>
                   <el-col :span="8">
-                    <el-select v-model="val2" placeholder="楼栋">
-                      <el-option label="区域一" value="shanghai"></el-option>
-                      <el-option label="区域二" value="beijing"></el-option>
+                    <el-select
+                      v-model="towerOptData"
+                      placeholder="栋座"
+                      clearable
+                      filterable
+                      remote
+                      :remote-method="queryRoomNo"
+                      @change="remoteRoomNoChange"
+                      :loading="towerLoading"
+                      value-key="value"
+                    >
+                      <el-option
+                        v-for="item in towerForList"
+                        :key="item.value"
+                        :label="item.name"
+                        :value="item"
+                      ></el-option>
                     </el-select>
                   </el-col>
                   <el-col :span="8">
-                    <el-select v-model="val3" placeholder="房间号">
-                      <el-option label="区域一" value="shanghai"></el-option>
-                      <el-option label="区域二" value="beijing"></el-option>
+                    <el-select
+                      v-model="roomOptData"
+                      placeholder="请输入房号"
+                      clearable
+                      filterable
+                      remote
+                      :remote-method="queryRoomData"
+                      @change="queryRoomDataChange"
+                      :loading="roomLoading"
+                      value-key="value"
+                    >
+                      <el-option
+                        v-for="item in roomForList"
+                        :key="item.value"
+                        :label="item.name"
+                        :value="item"
+                      ></el-option>
                     </el-select>
                   </el-col>
                 </el-form-item>
               </el-row>
             </el-col>
-            <el-col :span="8">
+            <el-col :span="6">
               <el-form-item label="房源编号">
-                <el-input v-model="val4" placeholder="请输入房源编号"></el-input>
+                <el-input
+                  v-model="conditions.houseNo"
+                  placeholder="请输入房源编号"
+                  @change="query(1)"
+                  clearable
+                ></el-input>
               </el-form-item>
             </el-col>
-            <el-col :span="8">
+            <el-col :span="6">
               <el-form-item label="锁定时间">
                 <el-date-picker
                   prefix-icon="prefix-icon"
-                  v-model="val5"
+                  v-model="conditions.timeSelect"
                   type="daterange"
                   range-separator="至"
                   start-placeholder="开始日期"
-                  end-placeholder="结束日期">
+                  end-placeholder="结束日期"
+                  value-format="yyyy-MM-dd"
+                  @change="query(1)"
+                  :default-time="['00:00:00', '23:59:59']"
+                >
                 </el-date-picker>
               </el-form-item>
             </el-col>
@@ -51,17 +103,17 @@
         </el-row>
       </div>
       <div class="conditions-btn">
-        <button class="btn">重置</button>
-        <button class="btn active">搜索</button>
+        <button class="btn" @click="reset">重置</button>
+        <button class="btn active" @click="query(1)">搜索</button>
       </div>
     </div>
     <div class="main">
       <div class="content">
         <div class="table">
           <el-table
-            :data="workData"
+            :data="tableData"
             height="100%"
-            @sort-change="changeWorkSort"
+            v-loading="loading"
           >
             <el-table-column
               v-for="(item, index) in workColumn"
@@ -75,25 +127,32 @@
               :sort-orders="['ascending', 'descending']"
               >
             </el-table-column>
-            <el-table-column
-              fixed="right"
-              label="操作"
-              width="100">
-              <template slot-scope="scope">
-                <el-button @click="handleClick(scope.row)" type="text" size="small">解锁</el-button>
-                <el-button type="text" size="small">查看</el-button>
+            <el-table-column label="操作" fixed="right" min-width="150">
+              <template v-slot="scope">
+                <el-button
+                  type="text"
+                  size="mini"
+                  @click="unLock(scope.row.id)"
+                  >解锁</el-button
+                >
+                <el-button
+                  type="text"
+                  size="mini"
+                  @click="toLook(scope.row.id)"
+                  >查看</el-button
+                >
               </template>
             </el-table-column>
           </el-table>
         </div>
         <el-pagination
-        @size-change="handleSizeChange($event, 'workPaginate')"
-        @current-change="handleCurrentChange($event, 'workPaginate')"
-        :current-page="workPaginate.page"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pageJson.page"
         :page-sizes="[5, 10, 15]"
-        :page-size="workPaginate.limit"
+        :page-size="pageJson.limit"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="workPaginate.total">
+        :total="pageJson.total">
         </el-pagination>
       </div>
     </div>
@@ -101,129 +160,337 @@
 </template>
 <script>
 import tabs from "./components/tabs.vue";
+import util from "@/util/util";
 export default {
   components: { tabs },
   data() {
     return {
-      val1: "",
-      val2: "",
-      val3: "",
-      val4: "",
-      val5: "",
-      workData: [],
+      loading: false,
+      conditions: {
+        comId: "",
+        cbId: "",
+        bhId: "",
+        houseNo: "",
+        timeSelect: ""
+      },
+      buildLoading: false, //楼盘select loading
+      buildOptData: {}, //当前楼盘选择数据
+      buildForList: [], //楼盘select数据
+      towerLoading: false, //栋座select loading
+      towerOptData: {}, //栋座选中数据
+      towerForList: [], //栋座select数据
+      roomLoading: false, //房间号select loading
+      roomOptData: {}, //房间号选中数据
+      roomForList: [], //房间号select数据
+      tableData: [],
       workColumn: [
         {
-          prop: "",
+          prop: "houseNo",
           label: "房源编号",
-          minWidth: "120",
+          minWidth: "270",
           align: "left"
         }, {
-          prop: "",
+          prop: "communityName",
           label: "小区名称",
           minWidth: "150",
-          align: "right",
-          sortable: true
+          align: "right"
         }, {
-          prop: "",
+          prop: "buildingName",
           label: "楼栋名称",
-          minWidth: "150",
-          align: "right",
-          sortable: true
+          minWidth: "100",
+          align: "right"
         }, {
-          prop: "",
+          prop: "roomNo",
           label: "房间号",
-          minWidth: "150",
-          align: "right",
-          sortable: true
+          minWidth: "100",
+          align: "right"
         }, {
-          prop: "",
+          prop: "plate",
           label: "状态",
           minWidth: "150",
-          align: "right",
-          sortable: true
+          align: "right"
         }, {
-          prop: "",
+          prop: "agentName",
           label: "跟单人",
-          minWidth: "150",
-          align: "right",
-          sortable: true
+          minWidth: "120",
+          align: "right"
         }, {
-          prop: "",
+          prop: "lockName",
           label: "锁定人",
-          minWidth: "150",
-          align: "right",
-          sortable: true
+          minWidth: "120",
+          align: "right"
         }, {
-          prop: "",
+          prop: "lockTime",
           label: "锁定时间",
-          minWidth: "150",
-          align: "right",
-          sortable: true
+          minWidth: "200",
+          align: "right"
         }
       ],
-      workPaginate: {
+      pageJson: {
         page: 1,
-        limit: 5,
+        limit: 10,
         total: 0,
         pageSum: 0
-      }
+      },
+      sortColumn: "id", //排序字段
+      sortType: "descending", //排序类型
     }
   },
+  created() {
+    this.query();
+  },
   methods: {
-    /**
-     * @example: 作业数据排序变化触发事件
-     */
-    changeWorkSort({column, prop, order}) {
-      this.workSortColumn = prop;
-      this.workSortType = order=="ascending" ? 0 : 1;
-      Object.assign(this.workPaginate, this.$options.data().workPaginate);
-      this.getWorkData();
-    },
     /**
      * @example: 改变每页请求数据数量
      * @param {val} 请求数
      * @param {type} 分页类型
      */
-    handleSizeChange(val, type) {
-      this[type].limit = val;
-      switch(type) {
-        case "workPaginate":
-          this.getWorkData();
-          break;
-        case "developPaginate":
-          this.getDevelopData();
-          break;
-        case "currencyPaginate":
-          this.getCurrencyData();
-          break;
-      }
+    handleSizeChange(val) {
+      this.pageJson.limit = val;
+      this.query();
     },
     /**
      * @example: 改变分页当前页码
      * @param {val} 页码
      * @param {type} 分页类型
      */
-    handleCurrentChange(val, type) {
-      this[type].page = val;
-      switch(type) {
-        case "workPaginate":
-          this.getWorkData();
-          break;
-        case "developPaginate":
-          this.getDevelopData();
-          break;
-        case "currencyPaginate":
-          this.getCurrencyData();
-          break;
-      }
+    handleCurrentChange(val) {
+      this.pageJson.page = val;
+      this.query(val);
     },
-    handleClick(row) {
-      console.log(row);
+    /**
+     * @example: 楼盘激活第一时获取数据
+     */
+    remoteBuildInput() {
+      // this.buildForList.length === 0 && this.buildRemoteMethod();
+      this.buildRemoteMethod();
+    },
+    /**
+     * @example: 远程获取楼盘信息
+     */
+    buildRemoteMethod(query) {
+      this.buildLoading = true;
+      this.$api
+      .get({
+        url: "/community/saleAll",
+        headers: { "Content-Type": "application/json;charset=UTF-8" },
+        token: false,
+        qs: true,
+        data: {
+          communityName: query,
+          page: 1,
+          limit: 50
+        }
+      })
+      .then(e => {
+        if (e.data.code == 200) {
+          this.buildForList = e.data.data.list;
+        }
+      })
+      .finally(() => {
+        this.buildLoading = false;
+      });
+    },
+    /**
+     * @example: 楼盘选择更改触发事件
+     * @param {Obejct} item 当前选中对象
+     */
+    remoteBuildChange(item) {
+      const { name = undefined, value = undefined } = item;
+
+      //如果删除或者手动删除传入空字符串将会把楼栋数据清理为空字符串
+      this.conditions.comId = value ? value : "";
+
+      //清理楼栋数据
+      this.towerForList = [];
+      this.conditions.cbId = "";
+      this.towerOptData = {};
+
+      //清理房间号数据
+      this.conditions.bhId = "";
+      this.roomOptData = {};
+      this.roomForList = [];
+
+      this.query();
+      //获取楼栋select
+      this.queryRoomNo();
+    },
+    /**
+     * @example: 获取栋座远程数据
+     * @param {String} name 栋座名称
+     */
+    queryRoomNo(name) {
+      this.towerLoading = true;
+      this.$api
+        .get({
+          url: "/mateHouse/queryComBuilding",
+          headers: { "Content-Type": "application/json;charset=UTF-8" },
+          token: false,
+          qs: true,
+          data: {
+            comId: this.conditions.comId,
+            page: 1,
+            limit: 100
+          }
+        })
+        .then(e => {
+          if (e.data.code == 200) {
+            this.towerForList = e.data.data.list;
+          }
+        })
+        .finally(() => {
+          this.towerLoading = false;
+        });
+    },
+    /**
+     * @example: 楼栋选择更改触发事件
+     * @param {Obejct} item 当前选中对象
+     */
+    remoteRoomNoChange(item) {
+      const { name = undefined, value = undefined } = item;
+
+      //如果删除或者手动删除传入空字符串将会把楼栋数据清理为空字符串
+      this.conditions.cbId = value ? value : "";
+
+      //清理房间号数据
+      this.conditions.bhId = "";
+      this.roomOptData = {};
+      this.roomForList = [];
+
+      this.query();
+      //获取房间号数据
+      this.queryRoomData();
+    },
+    /**
+     * @example: 远程获取房间号信息
+     * @param {String} e 输入搜索的文本
+     */
+    queryRoomData(e) {
+      this.$api
+        .get({
+          url: "/mateHouse/queryBuildIngHouses",
+          headers: { "Content-Type": "application/json;charset=UTF-8" },
+          token: false,
+          qs: true,
+          data: {
+            comId: this.conditions.comId,
+            cbId: this.conditions.cbId,
+            page: 1,
+            limit: 500,
+            roomNo: e == undefined ? "" : e.trim()
+          }
+        })
+        .then(e => {
+          if (e.data.code == 200) {
+            this.roomForList = e.data.data.list;
+          }
+        })
+        .finally(() => {
+          this.towerLoading = false;
+        });
+    },
+    /**
+     * @example:房间号修改选中触发事件
+     * @param {Ojbect} item 选中时选中的数据
+     */
+    queryRoomDataChange(item) {
+      const { name = undefined, value = undefined } = item;
+
+      this.conditions.bhId = value ? value : "";
+      this.query();
+    },
+    /**
+     * @example: 查询条件重置
+     */
+    reset() {
+      Object.assign(this.$data, this.$options.data());
+      this.query();
+    },
+    /**
+     * @example: 搜索
+     */
+    query(currentPage=1) {
+      this.pageJson.page = currentPage;
+      this.loading = true;
+      let params = { limit: this.pageJson.limit, page: currentPage };
+      params.comId = this.conditions.comId;
+      params.cbId = this.conditions.cbId;
+      params.bhId = this.conditions.bhId;
+      params.beginTime = this.conditions.timeSelect[0];
+      params.endTime = this.conditions.timeSelect[1];
+      params.houseNo = this.conditions.houseNo;
+      params.sortColumn = this.sortColumn;
+      params.sortType = this.sortType;
+      this.$api
+        .post({
+          headers: { "Content-Type": "application/json;charset=UTF-8" },
+          url: "/lockingHouse/lockedList",
+          data: params
+        })
+        .then(e => {
+          let data = e.data;
+          if (data.code == 200) {
+            this.pageJson.total = data.data.data.totalCount;
+            this.tableData = data.data.data.list;
+            // let btnList = data.data.btnList;
+          }
+        })
+        .catch(e => {
+          console.log("查询锁定房源列表失败");
+          console.log(e);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    /**
+     * @example: 解锁
+     */
+    unLock(id) {
+      let that = this;
+      let params = {
+        Eid: id,
+        Islocking: 0
+      };
+      that.loading = true;
+      this.$api
+        .post({
+          url: "/agentHouse/property/locking",
+          data: params,
+          headers: { "Content-Type": "application/json;charset=UTF-8" },
+          token: false
+        })
+        .then(e => {
+          let result = e.data;
+          that.$message(result.message);
+          if (result.code == 200) {
+            this.query(1);
+          }
+          that.loading = false;
+        })
+        .catch(e => {});
+    },
+    /**
+     * @example: 查看
+     */
+    toLook(id) {
+      var that = this;
+      util.openPage.call(this, {
+        name: "houseDetails",
+        params: { houseId: id }
+      });
     }
   }
 }
 </script>
 <style lang="less" scoped>
+.el-select-dropdown__item {
+  // prettier-ignore
+  height: 40PX;
+  // prettier-ignore
+  line-height: 40PX;
+  font-size: @font14;
+}
 .locking-container {
   display: flex;
   flex-direction: column;
@@ -360,9 +627,11 @@ export default {
                 padding-left: 16PX;
               }
             }
-            .cell:last-child {
-              // prettier-ignore
-              padding-right: 16PX;
+            td:last-child {
+              .cell {
+                // prettier-ignore
+                padding-right: 16PX;
+              }
             }
           }
         }
@@ -390,6 +659,11 @@ export default {
         .el-table__body td {
           // perttier-ignore
           height: 64PX;
+        }
+        .el-button--mini, .el-button--small {
+          // prettier-ignore
+          padding: 0 10PX;
+          font-size: @font16;
         }
       }
       .el-pagination {
