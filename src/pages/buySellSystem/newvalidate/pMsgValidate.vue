1b2a39baf2a2245c19eb96b43fc6b982db892a3a
ljt <410039324@qq.com>
2020-09-11 12:04:31 +0800

    验真方式详情post改get

--- src/pages/buySellSystem/newvalidate/pMsgValidate.vue
+++ src/pages/buySellSystem/newvalidate/pMsgValidate.vue
@@ -688,7 +688,7 @@ export default {
     },
     getDetail() {
       return this.$api
-        .post({
+        .get({
           url: `/validate/agent_house/getHouseModeDetail/${this.id}`
         })
         .then(({ data }) => {
