94017abc9367ff5947c2df05704f63f1234185b0
hxf <hxf@qq.com>
2020-10-10 09:27:10 +0800

    vr拍摄结果加"制作中"

--- src/pages/buySellSystem/myHouse/vrShooting/components/vrShootingPair.vue
+++ src/pages/buySellSystem/myHouse/vrShooting/components/vrShootingPair.vue
@@ -290,6 +290,7 @@ const SEARCHTABLIST = [
   { title: "全部", value: "" },
   { title: "拍摄中", value: 0 },
   { title: "拍摄完成", value: 1 },
+  { title: "制作中", value: 4 },
   { title: "已取消", value: 2 },
   { title: "已失败", value: 3 }
 ];
