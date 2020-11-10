f0b3c779428adfd217a7d9e9423743938c7314ee
hxf <hxf@qq.com>
2020-11-10 17:49:04 +0800

    扫码上传图片和视频

--- src/pages/buySellSystem/houseDetails/newDidLog/interviewPop.vue
+++ src/pages/buySellSystem/houseDetails/newDidLog/interviewPop.vue
@@ -1096,8 +1096,8 @@ export default {
       //webSocketUser:默认是二维码标识，可以不传。发消息就是基于这个标识发送的；如果一个页面有多个二维码需要自己生成全球唯一（见guid()函数实例）
       this.$api
         .post({
-          url: "/scanUpload/getUploadQrCodeMulti1",
-          data: { remark: "请选择文件" },
+          url: "/scanUpload/getUploadQrCode",
+          data: { remark: "请选择文件", resourceType: "picture,vedio" },
           headers: { "Content-Type": "application/json" }
         })
         .then(e => {
